9e9f831a757af01cc3b1edfe590e27f7ab53c2ce
Huang Rui <bjhuangr@cn.ibm.com>
2016-10-20 14:07:37 +0800

    define interfaces

--- zvmsdk/vmops.py
+++ zvmsdk/vmops.py
@@ -4,7 +4,115 @@ from log import LOG
 import utils as zvmutils
 
 
+VMOPS = None
+
+
+def _get_vmops():
+    if VMOPS is None:
+        VMOPS = VMOps()
+    return VMOPS
+
+
+def run_instance(instance_name, image_id, cpu, memory,
+                 login_password, ip_addr):
+    """Deploy and provision a virtual machine.
+
+    Input parameters:
+    :instance_name:   USERID of the instance, last 8 if length > 8
+    :image_id:        Image ID
+    :cpu:             vcpu
+    :memory:          memory
+    :login_password:  login password
+    :ip_addr:         ip address
+    """
+    pass
+
+
+def terminate_instance(instance_name):
+    """Destroy a virtual machine.
+
+    Input parameters:
+    :instance_name:   USERID of the instance, last 8 if length > 8
+    """
+    pass
+
+def start_instance(instance_name):
+    """Power on a virtual machine.
+
+    Input parameters:
+    :instance_name:   USERID of the instance, last 8 if length > 8
+    """
+    _get_vmops()._power_state(instance_name, "PUT", "on")
+
+def stop_instance(instance_name):
+    """Shutdown a virtual machine.
+
+    Input parameters:
+    :instance_name:   USERID of the instance, last 8 if length > 8
+    """
+    pass
+
+
+def create_volume(volume_name, size):
+    """Create a volume.
+
+    Input parameters:
+    :volume_name:     volume name
+    :size:            size
+    """
+    pass
+
+
+def delete_volume(volume_name):
+    """Create a volume.
+
+    Input parameters:
+    :volume_name:     volume name
+    """
+    pass
+
+
+def attach_volume(instance_name, volume_name):
+    """Create a volume.
+
+    Input parameters:
+    :instance_name:   USERID of the instance, last 8 if length > 8
+    :volume_name:     volume name
+    """
+    pass
+
+
+def capture_instance(instance_name, image_name):
+    """Caputre a virtual machine image.
+
+    Input parameters:
+    :instance_name:   USERID of the instance, last 8 if length > 8
+    :image_name:      Image name
+    """
+    pass
+
+
+def delete_image(image_name):
+    """Delete image.
+
+    Input parameters:
+    :image_name:      Image name
+    """
+    pass
+
+
+def detach_volume(instance_name, volume_name):
+    """Create a volume.
+
+    Input parameters:
+    :instance_name:   USERID of the instance, last 8 if length > 8
+    :volume_name:     volume name
+    """
+    pass
+
+
 class VMOps(object):
+
     def __init__(self):
         self._xcat_url = zvmutils.get_xcat_url()
 
