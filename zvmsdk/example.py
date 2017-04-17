426d04e0ccc827a7b30c88523ad41f65be3c7923
zhangleizlzl <zleizlbj@cn.ibm.com>
2017-04-17 14:47:01 +0800

    the structure of zvm_client. We add three 'py' files: interface, client and example。 the example.py show you how to use a ZVM client
    Change-Id: I1ad6494ed801cf8525285b5e890796cd9474a937

---
+++ zvmsdk/example.py
@@ -0,0 +1,56 @@
+"""
+    HOW TO USE ZVM CLIENT
+    example: "create_userid" and "_add_nic_to_table" implemented through ZVM CLIENT
+"""
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import client as zvmclient
+from zvmsdk import config
+from zvmsdk import log
+
+CONF = config.CONF
+LOG = log.LOG
+
+
+def create_userid(self, instance_name, cpu, memory, image_name):
+    """
+    Create z/VM userid into user directory for a z/VM instance.
+    """
+    LOG.debug("Creating the z/VM user entry for instance %s"
+              % instance_name)
+
+    # step1-1: create a object:client, which use the default interface
+    # in configureation files
+    client = zvmclient.get_client()
+
+    # or we can assign a interface type
+    # step1-2:another way  if we don't use the type in CONF
+    # client = zvmclient.ZVMClient('libvirt')
+
+    try:
+        # step2: call "request" method to interact with ZVM
+        result = client.create_request(func_name='create_userid_request',
+                                       instance_name=instance_name,
+                                       cpu=cpu,
+                                       memory=memory,
+                                       image_name=image_name)
+
+        # step3: process the returned result
+        if result is True:
+            # success
+            pass
+        else:
+            # failure
+            pass
+
+        size = CONF.root_disk_units
+        # Add root disk and set ipl
+        self.add_mdisk(instance_name, CONF.zvm_diskpool,
+                       CONF.zvm_user_root_vdev, size)
+        self.set_ipl(instance_name, CONF.zvm_user_root_vdev)
+
+    except Exception as err:
+        msg = ("Failed to create z/VM userid: %s" % err)
+        LOG.error(msg)
+        raise ZVMException(msg=err)
