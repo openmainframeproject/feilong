11448433417f5da5009ddafb7fa31eeaf4b22f40
dongyan yang <dyyang@cn.ibm.com>
2017-04-24 17:38:52 +0800

    add get_host_info and get_diskpool_info
    add two exposed APIs: get_host_info and get_diskpool_info
    add hostops.py to contain host related API implementation
    
    Change-Id: I1f6349c6c05e40bb6a2d62a80739ef3c7e70725c

--- tests/unit/test_zvmsdk.py
+++ tests/unit/test_zvmsdk.py
@@ -4,13 +4,14 @@ import unittest
 
 from zvmsdk.config import CONF
 import zvmsdk.utils as zvmutils
-import zvmsdk.vmops as sdkapi
+from zvmsdk import vmops
+from zvmsdk import hostops
 
 
 class SDKTestCase(unittest.TestCase):
     def setUp(self):
-        self.vmops = sdkapi._get_vmops()
-        self.volumeops = sdkapi._get_volumeops()
+        self.vmops = vmops.get_vmops()
+        self.hostops = hostops.get_hostops()
 
     def _fake_fun(self, value = None):
         return lambda *args, **kwargs: value
@@ -70,7 +71,7 @@ class SDKVMOpsTestCase(SDKTestCase):
         self.assertEqual(ret, True)
 
     @mock.patch('zvmsdk.client.XCATClient._power_state')
-    def test_power_on(self, power_state, ):
+    def test_power_on(self, power_state):
         self.vmops.power_on('cbi00063')
         power_state.assert_called_once_with('cbi00063', 'PUT', 'on')
 
@@ -188,6 +189,60 @@ class SDKVMOpsTestCase(SDKTestCase):
 class SDKVolumeOpsTestCase(SDKTestCase):
     def test_temp(self):
         pass
+
+
+class SDKHostOpsTestCase(SDKTestCase):
+    def _fake_host_rinv_info(self):
+        fake_host_rinv_info = ["fakenode: z/VM Host: FAKENODE\n"
+                               "fakenode: zHCP: fakehcp.fake.com\n"
+                               "fakenode: CEC Vendor: FAKE\n"
+                               "fakenode: CEC Model: 2097\n"
+                               "fakenode: Hypervisor OS: z/VM 6.1.0\n"
+                               "fakenode: Hypervisor Name: fakenode\n"
+                               "fakenode: Architecture: s390x\n"
+                               "fakenode: LPAR CPU Total: 10\n"
+                               "fakenode: LPAR CPU Used: 10\n"
+                               "fakenode: LPAR Memory Total: 16G\n"
+                               "fakenode: LPAR Memory Offline: 0\n"
+                               "fakenode: LPAR Memory Used: 16.0G\n"
+                               "fakenode: IPL Time:"
+                               "IPL at 03/13/14 21:43:12 EDT\n"]
+        return {'info': [fake_host_rinv_info, ]}
+
+    def _fake_disk_info(self):
+        fake_disk_info = ["fakenode: FAKEDP Total: 406105.3 G\n"
+                          "fakenode: FAKEDP Used: 367262.6 G\n"
+                          "fakenode: FAKEDP Free: 38842.7 G\n"]
+        return {'info': [fake_disk_info, ]}
+
+    @mock.patch('zvmsdk.client.XCATClient.get_diskpool_info')
+    @mock.patch.object(zvmutils, 'xcat_request')
+    def test_get_host_info(self, xrequest, get_diskpool_info):
+        xrequest.return_value = self._fake_host_rinv_info()
+        get_diskpool_info.return_value = {'disk_total': 100, 'disk_used': 80,
+                                          'disk_available': 20}
+        host_info = self.hostops.get_host_info('fakenode')
+        self.assertEqual(host_info['vcpus'], 10)
+        self.assertEqual(host_info['hypervisor_version'], 610)
+        self.assertEqual(host_info['disk_total'], 100)
+        url = "/xcatws/nodes/fakenode/inventory?userName=" +\
+                CONF.xcat.username + "&password=" +\
+                CONF.xcat.password + "&format=json"
+        xrequest.assert_called_once_with('GET', url)
+        get_diskpool_info.assert_called_once_with('fakenode')
+
+    @mock.patch.object(zvmutils, 'xcat_request')
+    def test_get_diskpool_info(self, xrequest):
+        xrequest.return_value = self._fake_disk_info()
+        dp_info = self.hostops.get_diskpool_info('fakenode', 'FAKEDP')
+        url = "/xcatws/nodes/fakenode/inventory?userName=" +\
+                CONF.xcat.username + "&password=" +\
+                CONF.xcat.password + "&format=json" +\
+                "&field=--diskpoolspace&field=FAKEDP"
+        xrequest.assert_called_once_with('GET', url)
+        self.assertEqual(dp_info['disk_total'], 406105)
+        self.assertEqual(dp_info['disk_used'], 367263)
+        self.assertEqual(dp_info['disk_available'], 38843)
 
 
 if __name__ == '__main__':
---
+++ zvmsdk/hostops.py
@@ -0,0 +1,42 @@
+# Copyright 2017 IBM Corp.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+
+from zvmsdk import client as zvmclient
+from zvmsdk import config
+from zvmsdk import log
+
+
+_HOSTOPS = None
+CONF = config.CONF
+LOG = log.LOG
+
+
+def get_hostops():
+    global _HOSTOPS
+    if _HOSTOPS is None:
+        _HOSTOPS = HOSTOps()
+    return _HOSTOPS
+
+
+class HOSTOps(object):
+
+    def __init__(self):
+        self._zvmclient = zvmclient.get_zvmclient()
+
+    def get_host_info(self, host):
+        return self._zvmclient.get_host_info(host)
+
+    def get_diskpool_info(self, host, pool=CONF.zvm.diskpool):
+        return self._zvmclient.get_diskpool_info(host, pool)
