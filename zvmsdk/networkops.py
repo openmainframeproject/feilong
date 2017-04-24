1c9ad694c36ee5c96109f82683076ac0aa6f27e5
PanYaLian <ylpan@cn.ibm.com>
2017-04-24 17:38:44 +0800

    Add networkops, submit clean_mac_switch and clean_mac_switch_host API
    Change-Id: If6d88a35b3e9658defe87d028548bc7bdee5f39f

---
+++ zvmsdk/networkops.py
@@ -0,0 +1,47 @@
+# Copyright 2017 IBM Corp.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+
+
+from zvmsdk import client as zvmclient
+from zvmsdk import config
+from zvmsdk import log
+
+
+_NetworkOPS = None
+
+
+def _get_networkops():
+    global _NetworkOPS
+    if _NetworkOPS is None:
+        _NetworkOPS = NetworkOPS()
+    return _NetworkOPS
+
+
+class NetworkOPS(object):
+    """Configuration check and manage MAC address API
+       oriented towards SDK driver
+    """
+    def __init__(self):
+        self.zvmclient = zvmclient.get_zvmclient()
+
+    def clean_mac_switch_host(self, node_name):
+        """Clean node records, including mac, host and switch table."""
+        self.clean_mac_switch(node_name)
+        self.zvmclient.delete_host(node_name)
+
+    def clean_mac_switch(self, node_name):
+        """Clean node records, including mac and switch table."""
+        self.zvmclient.delete_mac(node_name)
+        self.zvmclient.delete_switch(node_name)
