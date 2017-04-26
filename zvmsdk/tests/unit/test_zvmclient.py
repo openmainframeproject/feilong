96f5b86ade00fb2e90a808c4fed1d762516ce1a7
dongyan yang <dyyang@cn.ibm.com>
2017-04-26 15:24:37 +0800

    Reorganize the UT cases for get_host_info and get_diskpool_info
    Divide the test cases into test_zvmclient.py and test_hostops.py
    
    Change-Id: I042c91e80d98616480ec97dd5de88c4c457cf870

--- zvmsdk/tests/unit/test_zvmclient.py
+++ zvmsdk/tests/unit/test_zvmclient.py
@@ -29,6 +29,66 @@ class SDKZVMClientTestCase(base.SDKTestCase):
         if CONF.zvm.client_type == 'xcat':
             self.assertTrue(isinstance(self._zvmclient, zvmclient.XCATClient))
 
+    def _fake_host_rinv_info(self):
+        fake_host_rinv_info = ["fakenode: z/VM Host: FAKENODE\n"
+                               "fakenode: zHCP: fakehcp.fake.com\n"
+                               "fakenode: CEC Vendor: FAKE\n"
+                               "fakenode: CEC Model: 2097\n"
+                               "fakenode: Hypervisor OS: z/VM 6.1.0\n"
+                               "fakenode: Hypervisor Name: fakenode\n"
+                               "fakenode: Architecture: s390x\n"
+                               "fakenode: LPAR CPU Total: 10\n"
+                               "fakenode: LPAR CPU Used: 10\n"
+                               "fakenode: LPAR Memory Total: 16G\n"
+                               "fakenode: LPAR Memory Offline: 0\n"
+                               "fakenode: LPAR Memory Used: 16.0G\n"
+                               "fakenode: IPL Time:"
+                               "IPL at 03/13/14 21:43:12 EDT\n"]
+        return {'info': [fake_host_rinv_info, ]}
+
+    def _fake_disk_info(self):
+        fake_disk_info = ["fakenode: FAKEDP Total: 406105.3 G\n"
+                          "fakenode: FAKEDP Used: 367262.6 G\n"
+                          "fakenode: FAKEDP Free: 38842.7 G\n"]
+        return {'info': [fake_disk_info, ]}
+
+    @mock.patch.object(zvmclient.XCATClient, '_construct_zhcp_info')
+    @mock.patch.object(zvmclient.XCATClient, 'get_diskpool_info')
+    @mock.patch.object(zvmutils, 'xcat_request')
+    def test_get_host_info(self, xrequest, get_diskpool_info,
+                           _construct_zhcp_info):
+        xrequest.return_value = self._fake_host_rinv_info()
+        get_diskpool_info.return_value = {'disk_total': 100, 'disk_used': 80,
+                                          'disk_available': 20}
+        fake_zhcp_info = {'hostname': 'fakehcp.fake.com',
+                          'nodename': 'fakehcp',
+                          'userid': 'fakehcp'}
+        _construct_zhcp_info.return_value = fake_zhcp_info
+        host_info = self._zvmclient.get_host_info('fakenode')
+        self.assertEqual(host_info['vcpus'], 10)
+        self.assertEqual(host_info['hypervisor_version'], 610)
+        self.assertEqual(host_info['disk_total'], 100)
+        self.assertEqual(self._zvmclient._zhcp_info, fake_zhcp_info)
+        url = "/xcatws/nodes/fakenode/inventory?userName=" +\
+                CONF.xcat.username + "&password=" +\
+                CONF.xcat.password + "&format=json"
+        xrequest.assert_called_once_with('GET', url)
+        get_diskpool_info.assert_called_once_with('fakenode')
+        _construct_zhcp_info.assert_called_once_with("fakehcp.fake.com")
+
+    @mock.patch.object(zvmutils, 'xcat_request')
+    def test_get_diskpool_info(self, xrequest):
+        xrequest.return_value = self._fake_disk_info()
+        dp_info = self._zvmclient.get_diskpool_info('fakenode', 'FAKEDP')
+        url = "/xcatws/nodes/fakenode/inventory?userName=" +\
+                CONF.xcat.username + "&password=" +\
+                CONF.xcat.password + "&format=json" +\
+                "&field=--diskpoolspace&field=FAKEDP"
+        xrequest.assert_called_once_with('GET', url)
+        self.assertEqual(dp_info['disk_total'], 406105)
+        self.assertEqual(dp_info['disk_used'], 367263)
+        self.assertEqual(dp_info['disk_available'], 38843)
+
     @mock.patch.object(zvmclient.XCATClient, 'get_host_info')
     @mock.patch.object(zvmclient.XCATClient, '_construct_zhcp_info')
     def test_get_hcp_info(self, _construct_zhcp_info, get_host_info):
