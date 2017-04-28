709174952e009e6e9be169f4ad0cd53e5b072e25
jichenjc <jichenjc@cn.ibm.com>
2017-04-29 06:10:21 +0800

    initial version of rest API handlers
    initial version of rest API handlers
    
    Partial-implement-blueprint rest_api_support
    
    Change-Id: Ibdc8559bcbe5fb57a2c2a9c50a82d210262b03a4

---
+++ zvmsdk/api/rest_api_version_history.rst
@@ -0,0 +1,7 @@
+REST API Version History
+~~~~~~~~~~~~~~~~~~~~~~~~
+
+This documents the changes made to the REST API with every
+microversion change. The description for each version should be a
+verbose one which has enough information to be suitable for use in
+user documentation.
---
+++ zvmsdk/api/util.py
@@ -0,0 +1,133 @@
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+"""Utility methods for placement API."""
+
+import functools
+import json
+
+import jsonschema
+import webob
+
+
+def loads(s, **kwargs):
+    return json.loads(s, **kwargs)
+
+
+def check_accept(*types):
+    """If accept is set explicitly, try to follow it.
+
+    If there is no match for the incoming accept header
+    send a 406 response code.
+
+    If accept is not set send our usual content-type in
+    response.
+    """
+    def decorator(f):
+        @functools.wraps(f)
+        def decorated_function(req):
+            if req.accept:
+                best_match = req.accept.best_match(types)
+                if not best_match:
+                    type_string = ', '.join(types)
+                    raise webob.exc.HTTPNotAcceptable(
+                        ('Only %(type)s is provided') % {'type': type_string},
+                        json_formatter=json_error_formatter)
+            return f(req)
+        return decorated_function
+    return decorator
+
+
+def extract_json(body, schema):
+    """Extract JSON from a body and validate with the provided schema."""
+    try:
+        data = loads(body)
+    except ValueError as exc:
+        raise webob.exc.HTTPBadRequest(
+            ('Malformed JSON: %(error)s') % {'error': exc},
+            json_formatter=json_error_formatter)
+    try:
+        jsonschema.validate(data, schema,
+                            format_checker=jsonschema.FormatChecker())
+    except jsonschema.ValidationError as exc:
+        raise webob.exc.HTTPBadRequest(
+            ('JSON does not validate: %(error)s') % {'error': exc},
+            json_formatter=json_error_formatter)
+    return data
+
+
+def json_error_formatter(body, status, title, environ):
+    """A json_formatter for webob exceptions.
+
+    Follows API-WG guidelines at
+    http://specs.openstack.org/openstack/api-wg/guidelines/errors.html
+    """
+    # Clear out the html that webob sneaks in.
+    body = webob.exc.strip_tags(body)
+    # Get status code out of status message. webob's error formatter
+    # only passes entire status string.
+    status_code = int(status.split(None, 1)[0])
+    error_dict = {
+        'status': status_code,
+        'title': title,
+        'detail': body
+    }
+    # If the request id middleware has had a chance to add an id,
+    # put it in the error response.
+
+    # When there is a no microversion in the environment and a 406,
+    # microversion parsing failed so we need to include microversion
+    # min and max information in the error response.
+    microversion = api.microversion
+    if status_code == 406 and microversion.MICROVERSION_ENVIRON not in environ:
+        error_dict['max_version'] = microversion.max_version_string()
+        error_dict['min_version'] = microversion.min_version_string()
+
+    return {'errors': [error_dict]}
+
+
+def require_content(content_type):
+    """Decorator to require a content type in a handler."""
+    def decorator(f):
+        @functools.wraps(f)
+        def decorated_function(req):
+            if req.content_type != content_type:
+                # webob's unset content_type is the empty string so
+                # set it the error message content to 'None' to make
+                # a useful message in that case. This also avoids a
+                # KeyError raised when webob.exc eagerly fills in a
+                # Template for output we will never use.
+                if not req.content_type:
+                    req.content_type = 'None'
+                raise webob.exc.HTTPUnsupportedMediaType(
+                    ('The media type %(bad_type)s is not supported, '
+                      'use %(good_type)s') %
+                    {'bad_type': req.content_type,
+                     'good_type': content_type},
+                    json_formatter=json_error_formatter)
+            else:
+                return f(req)
+        return decorated_function
+    return decorator
+
+
+def wsgi_path_item(environ, name):
+    """Extract the value of a named field in a URL.
+
+    Return None if the name is not present or there are no path items.
+    """
+    # NOTE(cdent): For the time being we don't need to urldecode
+    # the value as the entire placement API has paths that accept no
+    # encoded values.
+    try:
+        return environ['wsgiorg.routing_args'][1][name]
+    except (KeyError, IndexError):
+        return None
