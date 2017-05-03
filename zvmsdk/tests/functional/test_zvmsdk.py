ccc939104d50a7312e360bcc98f21c39cf6b3db9
Huang Rui <bjhuangr@cn.ibm.com>
2017-05-03 16:06:02 +0800

    add fvt testcases for get_host_info and get_diskpool_info
    Change-Id: Ifcf8a637ff5e2e7c77d65b9cf63586ae73be23c0

--- zvmsdk/tests/functional/test_zvmsdk.py
+++ zvmsdk/tests/functional/test_zvmsdk.py
@@ -1,6 +1,74 @@
+# Copyright 2017 IBM Corp.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
 
 import unittest
 
+from zvmsdk import api
+from zvmsdk import config
+from zvmsdk import exception
 
-class SDKTestCase(unittest.TestCase):
-    pass
+
+CONF = config.CONF
+
+
+def set_conf(section, opt, value):
+    CONF[section][opt] = value
+
+
+class SDKAPITestCase(unittest.TestCase):
+
+    def __init__(self, methodName='runTest'):
+        super(SDKAPITestCase, self).__init__(methodName)
+        self.sdkapi = api.SDKAPI()
+
+    def test_get_host_info(self):
+        host_info = self.sdkapi.get_host_info()
+        self.assertTrue(isinstance(host_info.get('disk_available'), int))
+        self.assertTrue(isinstance(host_info.get('ipl_time'), unicode))
+        self.assertTrue(isinstance(host_info.get('vcpus_used'), int))
+        self.assertEqual(host_info.get('hypervisor_type'), 'zvm')
+        self.assertTrue(isinstance(host_info.get('disk_total'), int))
+        self.assertTrue(isinstance(host_info.get('hypervisor_hostname'),
+                                   unicode))
+        self.assertTrue(isinstance(host_info.get('memory_mb'), float))
+        self.assertTrue(isinstance(host_info.get('cpu_info'), dict))
+        self.assertTrue(isinstance(host_info.get('vcpus'), int))
+        self.assertTrue(isinstance(host_info.get('hypervisor_version'), int))
+        self.assertTrue(isinstance(host_info.get('disk_used'), int))
+        self.assertTrue(isinstance(host_info.get('memory_mb_used'), float))
+
+    def test_get_host_info_invalid_host(self):
+        zvm_host = CONF.zvm.host
+        self.addCleanup(set_conf, 'zvm', 'host', zvm_host)
+
+        CONF.zvm.host = 'invalidhost'
+        self.assertRaises(exception.SDKBaseException,
+                          self.sdkapi.get_host_info)
+
+    def test_get_diskpool_info(self):
+        disk_info = self.sdkapi.get_diskpool_info()
+        self.assertTrue(isinstance(disk_info.get('disk_available'), int))
+        self.assertTrue(isinstance(disk_info.get('disk_total'), int))
+        self.assertTrue(isinstance(disk_info.get('disk_used'), int))
+
+    def test_get_diskpool_info_with_parameter(self):
+        disk_info = self.sdkapi.get_diskpool_info('xcatfba1')
+        self.assertTrue(isinstance(disk_info.get('disk_available'), int))
+        self.assertTrue(isinstance(disk_info.get('disk_total'), int))
+        self.assertTrue(isinstance(disk_info.get('disk_used'), int))
+
+    def test_get_diskpool_info_invalid_diskpool(self):
+        self.assertRaises(exception.SDKBaseException,
+                          self.sdkapi.get_diskpool_info, 'invalidpoolname')
