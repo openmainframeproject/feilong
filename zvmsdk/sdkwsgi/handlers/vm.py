c0f9abe3cfd2530729c53860c34a3587818f53a4
jichenjc <jichenjc@cn.ibm.com>
2017-05-06 00:26:48 +0800

    enhance API
    enance API to make API runable
    
    Change-Id: I4ddada1b6a173fd27dc33cac37d2a645a8aaacf8

--- zvmsdk/sdkwsgi/handlers/vm.py
+++ zvmsdk/sdkwsgi/handlers/vm.py
@@ -11,25 +11,17 @@
 #    under the License.
 """Handler for the root of the sdk API."""
 
-import json
-
-from zvmsdk.sdkwsgi import microversion
+from zvmsdk import config
+from zvmsdk import log
 from zvmsdk.sdkwsgi import wsgi_wrapper
-from zvmsdk import utils
+from zvmsdk.sdkwsgi import tokens
+
+
+CONF = config.CONF
+LOG = log.LOG
 
 
 @wsgi_wrapper.SdkWsgify
-def home(req):
-    min_version = microversion.min_version_string()
-    max_version = microversion.max_version_string()
-
-    version_data = {
-        'id': 'v%s' % min_version,
-        'max_version': max_version,
-        'min_version': min_version,
-    }
-    version_json = json.dumps({'versions': [version_data]})
-    req.response.body = utils.to_utf8(version_json)
-    req.response.content_type = 'application/json'
-
-    return req.response
+@tokens.validate(req)
+def list_vm(req):
+    pass
