57253f5c6c94b6d419ac4ae3bfee6527342af543
jichenjc <jichenjc@cn.ibm.com>
2017-05-24 08:44:02 +0800

    Add host handler for /host
    Add host handler for /host
    
    Change-Id: I01a50943137946e37d9178ca027df4462c7063dc

---
+++ zvmsdk/sdkwsgi/handlers/host.py
@@ -0,0 +1,76 @@
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+"""Handler for the root of the sdk API."""
+
+from zvmsdk import config
+from zvmsdk import log
+from zvmsdk.sdkwsgi.handlers import tokens
+from zvmsdk.sdkwsgi import util
+from zvmsdk.sdkwsgi import wsgi_wrapper
+
+_HOSTACTION = None
+CONF = config.CONF
+LOG = log.LOG
+
+
+class HostAction(object):
+    def list(self, name):
+        LOG.info('list guest for a host')
+
+    def get_info(self, name):
+        LOG.info('get host info')
+
+    def get_disk_info(self, name, diskname):
+        LOG.info('get host disk info')
+
+
+def get_action():
+    global _HOSTACTION
+    if _HOSTACTION is None:
+        _HOSTACTION = HostAction()
+    return _HOSTACTION
+
+
+@wsgi_wrapper.SdkWsgify
+def host_list_guests(req):
+    tokens.validate(req)
+
+    def _host_list_guests(req):
+        action = get_action()
+        action.list()
+
+    name = util.wsgi_path_item(req.environ, 'name')
+    _host_list_guests(name)
+
+
+@wsgi_wrapper.SdkWsgify
+def host_get_info(req):
+
+    def _host_get_info(name):
+        action = get_action()
+        action.get_info(name)
+
+    name = util.wsgi_path_item(req.environ, 'name')
+    _host_get_info(name)
+
+
+@wsgi_wrapper.SdkWsgify
+def host_get_disk_info(req):
+
+    def _host_get_disk_info(name, diskname):
+        action = get_action()
+        action.get_disk_info(name, diskname)
+
+    name = util.wsgi_path_item(req.environ, 'name')
+    diskname = util.wsgi_path_item(req.environ, 'disk')
+
+    _host_get_disk_info(name, diskname)
---
+++ zvmsdk/tests/unit/sdkwsgi/handlers/test_host.py
@@ -0,0 +1,58 @@
+# Copyright 2017 IBM Corp.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import datetime
+import jwt
+import mock
+import unittest
+
+from zvmsdk.sdkwsgi.handlers import host
+
+
+class FakeReq(object):
+    def __init__(self):
+        self.headers = {}
+        self.environ = {}
+
+    def __getitem__(self, name):
+        return self.headers
+
+
+class HandlersGuestTest(unittest.TestCase):
+
+    def setUp(self):
+        expired_elapse = datetime.timedelta(seconds=100)
+        expired_time = datetime.datetime.utcnow() + expired_elapse
+        payload = jwt.encode({'exp': expired_time}, 'username')
+
+        self.req = FakeReq()
+        self.req.headers['X-Auth-Token'] = payload
+
+    @mock.patch.object(host.HostAction, 'list')
+    def test_host_list(self, mock_list):
+
+        host.host_list_guests(self.req)
+        self.assertTrue(mock_list.called)
+
+    @mock.patch.object(host.HostAction, 'get_info')
+    def test_host_get_info(self, mock_get_info):
+
+        host.host_get_info(self.req)
+        self.assertTrue(mock_get_info.called)
+
+    @mock.patch.object(host.HostAction, 'get_disk_info')
+    def test_host_get_disk_info(self, mock_get_disk_info):
+
+        host.host_get_disk_info(self.req)
+        self.assertTrue(mock_get_disk_info.called)
