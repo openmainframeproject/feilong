ae449d9c29ce66f69dac40d7267bf5993947aeb5
jichenjc <jichenjc@cn.ibm.com>
2017-05-26 03:33:57 +0800

    Add FaultWrapper
    if http return error or raise Exception, this wrapper will
    catch the exception and raise it.
    
    Change-Id: I1850e67c2a86f3fee0110bb5e7efe9b80c7ee22b

--- zvmsdk/sdkwsgi/deploy.py
+++ zvmsdk/sdkwsgi/deploy.py
@@ -11,6 +11,8 @@
 #    under the License.
 """Deployment handling for sdk API."""
 
+import webob
+
 from zvmsdk.sdkwsgi import handler
 from zvmsdk.sdkwsgi import microversion
 from zvmsdk.sdkwsgi import requestlog
@@ -19,14 +21,70 @@ from zvmsdk.sdkwsgi import requestlog
 NAME = "sdk"
 
 
+def walk_class_hierarchy(clazz, encountered=None):
+    """Walk class hierarchy, yielding most derived classes first."""
+    if not encountered:
+        encountered = []
+    for subclass in clazz.__subclasses__():
+        if subclass not in encountered:
+            encountered.append(subclass)
+            # drill down to leaves first
+            for subsubclass in walk_class_hierarchy(subclass, encountered):
+                yield subsubclass
+            yield subclass
+
+
+class FaultWrapper(object):
+    """Calls down the middleware stack, making exceptions into faults."""
+
+    _status_to_type = {}
+
+    @staticmethod
+    def status_to_type(status):
+        if not FaultWrapper._status_to_type:
+            for clazz in walk_class_hierarchy(webob.exc.HTTPError):
+                FaultWrapper._status_to_type[clazz.code] = clazz
+        return FaultWrapper._status_to_type.get(
+                                  status, webob.exc.HTTPInternalServerError)()
+
+    def __init__(self, application):
+        self.application = application
+
+    def _error(self, inner, req):
+
+        safe = getattr(inner, 'safe', False)
+        headers = getattr(inner, 'headers', None)
+        status = getattr(inner, 'code', 500)
+        if status is None:
+            status = 500
+
+        outer = self.status_to_type(status)
+        if headers:
+            outer.headers = headers
+
+        if safe:
+            outer.explanation = '%s: %s' % (inner.__class__.__name__,
+                                            inner.msg)
+
+        return outer
+
+    @webob.dec.wsgify()
+    def __call__(self, req):
+        try:
+            return req.get_response(self.application)
+        except Exception as ex:
+            return self._error(ex, req)
+
+
 def deploy(project_name):
     """Assemble the middleware pipeline leading to the placement app."""
     microversion_middleware = microversion.MicroversionMiddleware
     request_log = requestlog.RequestLog
-
+    fault_wrap = FaultWrapper
     application = handler.SdkHandler()
 
-    for middleware in (microversion_middleware,
+    for middleware in (fault_wrap,
+                       microversion_middleware,
                        request_log,
                        ):
         if middleware:
