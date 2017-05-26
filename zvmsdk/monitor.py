3adf5b6ba6166daab2d521000d88d507c2285239
dongyan yang <dyyang@cn.ibm.com>
2017-05-26 11:17:15 +0800

    implement cache layer for metering support
    Change-Id: If9e72e203819fd155a2f9cc442bab623a7ac5b86

---
+++ zvmsdk/monitor.py
@@ -0,0 +1,52 @@
+# Copyright 2017 IBM Corp.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import time
+
+
+class MeteringCache(object):
+    """Cache for metering data."""
+    _CTYPES = ('cpumem', 'vnics')
+
+    def __init__(self):
+        self._reset()
+
+    def _reset(self):
+        for type in self._CTYPES:
+            self.cache[type]['expiredate'] = time.time()
+            self.cache[type]['data'] = {}
+
+    def set(self, ctype, data):
+        """Set or update cache content.
+
+        @ctype:    cache type.
+        @data:    cache data.
+        """
+        self.cache[ctype]['data'][data['userid']] = data
+
+    def get(self, ctype, userid):
+        if(time.time() > self.cache[ctype]['expiredate']):
+            return None
+        else:
+            return self.cache[ctype]['data'].get(userid, None)
+
+    def delete(self, ctype, userid):
+        if userid in self.cache[ctype]['data']:
+            del self.cache[ctype]['data'][userid]
+
+    def clear(self, ctype='all'):
+        if ctype == 'all':
+            self._reset()
+        else:
+            self.cache[ctype]['data'] = {}
