0ce32faee049fa12a12528dbf048cc507b735165
dongyan yang <dyyang@cn.ibm.com>
2017-05-26 16:55:10 +0800

    enhance cache and implement inspect_cpus
    Change-Id: I98552f7de425f3fbcd4583f92521016d5863270d

--- zvmsdk/config.py
+++ zvmsdk/config.py
@@ -123,6 +123,12 @@ zvm_opts = [
         section='instance',
         default=100,
         opt_type='int'),
+    # monitor options
+    Opt('cache_interval',
+        section='monitor',
+        default=600,
+        opt_type='int',
+        ),
     ]
 
 
