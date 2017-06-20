fdf447edee58a8fa61ede6b8f665f3f9b6cd6b0a
Steven P. Gessner <gessners@us.ibm.com>
2017-06-20 15:27:23 -0400

    Initial code for Systems Management Ultra Thin Layer
    Change-Id: I24d12e175d6fa3c511afbf45bc162c07da2250e8

---
+++ smutLayer/deleteVM.py
@@ -0,0 +1,291 @@
+# DeleteVM functions for Systems Management Ultra Thin Layer
+#
+# Copyright 2017 IBM Corp.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import generalUtils
+from generalUtils import parseCmdline
+import subprocess
+from subprocess import CalledProcessError
+from vmUtils import invokeSMCLI
+
+version = "1.0.0"
+
+# List of subfunction handlers.
+# Each subfunction contains a list that has:
+#   Readable name of the routine that handles the subfunction,
+#   Code for the function call.
+subfuncHandler = {
+    'DIRECTORY': ['deleteMachine', lambda rh: deleteMachine(rh)],
+    'HELP': ['help', lambda rh: help(rh)],
+    'VERSION': ['getVersion', lambda rh: getVersion(rh)],
+    }
+
+"""
+List of positional operands based on subfunction.
+Each subfunction contains a list which has a dictionary with the following
+information for the positional operands:
+  - Human readable name of the operand,
+  - Property in the parms dictionary to hold the value,
+  - Is it required (True) or optional (False),
+  - Type of data (1: int, 2: string).
+"""
+posOpsList = {}
+
+"""
+List of additional operands/options supported by the various subfunctions.
+The dictionary followng the subfunction name uses the keyword from the
+command as a key.  Each keyword has a dictionary that lists:
+  - the related parms item that stores the value,
+  - how many values follow the keyword, and
+  - the type of data for those values (1: int, 2: string)
+"""
+keyOpsList = {
+    'DIRECTORY': {'--showparms': ['showParms', 0, 0]},
+    'HELP': {},
+    'VERSION': {},
+    }
+
+
+def deleteMachine(rh):
+    """
+    Delete a virtual machine from the user directory.
+
+    Input:
+       Request Handle with the following properties:
+          function    - 'DELETEVM'
+          subfunction - 'DIRECTORY'
+          userid      - userid of the virtual machine to be deleted.
+
+    Output:
+       Request Handle updated with the results.
+       Return code - 0: ok, non-zero: error
+    """
+
+    rh.printSysLog("Enter deleteVM.deleteMachine")
+
+    results = {'overallRC': 0}
+
+    # Is image logged on
+    state = 'off'
+    cmd = ("/sbin/vmcp q user " + rh.userid + " 2>/dev/null | " +
+              "sed 's/HCP\w\w\w045E.*/off/' | sed 's/HCP\w\w\w361E.*/off/' | "
+              + "sed 's/" + rh.userid + ".*/on/'")
+    try:
+        state = subprocess.check_output(
+                cmd,
+                stderr=subprocess.STDOUT,
+                close_fds=True,
+                shell=True)
+
+    except CalledProcessError as e:
+        # The last SED would have to fail for the exception to be thrown.
+        out = e.output
+        cmdRC = e.returncode
+        rh.printLn("ES", "Command failed: '" + cmd + "', rc: " +
+            str(cmdRC) + " out: " + out)
+        state = 'off'       # Treat state as off for this weird case
+
+    if state == 'on':
+        cmd = ["smcli",
+            "Image_Deactivate",
+            "-T", rh.userid,
+            "-f IMMED"]
+
+        results = invokeSMCLI(rh, cmd)
+        if results['overallRC'] != 0:
+            strCmd = ' '.join(cmd)
+            rh.printLn("ES", "Command failed: '" + strCmd +
+                "', out: '" + results['response'] +
+                "', rc: " + str(results['overallRC']))
+            rh.updateResults(results)
+
+    if results['overallRC'] == 0:
+        cmd = ["smcli",
+            "Image_Delete_DM",
+            "-T", rh.userid,
+            "-e", "0"]
+
+        results = invokeSMCLI(rh, cmd)
+        if results['overallRC'] != 0:
+            strCmd = ' '.join(cmd)
+            rh.printLn("ES",  "Command failed: '" + strCmd +
+                "', out: '" + results['response'] +
+                "', rc: " + str(results['overallRC']))
+            rh.updateResults(results)
+
+    rh.printSysLog("Exit deleteVM.deleteMachine, rc: " +
+        str(results['overallRC']))
+    return results['overallRC']
+
+
+def doIt(rh):
+    """
+    Perform the requested function by invoking the subfunction handler.
+
+    Input:
+       Request Handle
+
+    Output:
+       Request Handle updated with parsed input.
+       Return code - 0: ok, non-zero: error
+    """
+
+    rc = 0
+    rh.printSysLog("Enter deleteVM.doIt")
+
+    # Show the invocation parameters, if requested.
+    if 'showParms' in rh.parms and rh.parms['showParms'] is True:
+        rh.printLn("N", "Invocation parameters: ")
+        rh.printLn("N", "  Routine: deleteVM." +
+            str(subfuncHandler[rh.subfunction][0]) + "(reqHandle)")
+        rh.printLn("N", "  function: " + rh.function)
+        rh.printLn("N", "  userid: " + rh.userid)
+        rh.printLn("N", "  subfunction: " + rh.subfunction)
+        rh.printLn("N", "  parms{}: ")
+        for key in rh.parms:
+            if key != 'showParms':
+                rh.printLn("N", "    " + key + ": " +
+                    str(rh.parms[key]))
+        rh.printLn("N", " ")
+
+    # Call the subfunction handler
+    subfuncHandler[rh.subfunction][1](rh)
+
+    rh.printSysLog("Exit deleteVM.doIt, rc: " + str(rc))
+    return rc
+
+
+def getVersion(rh):
+    """
+    Get the version of this function.
+
+    Input:
+       Request Handle
+
+    Output:
+       Request Handle updated with the results.
+       Return code - 0: ok, non-zero: error
+    """
+
+    rh.printLn("N", "Version: " + version)
+    return 0
+
+
+def help(rh):
+    """
+    Produce help output specifically for DeleteVM functions.
+
+    Input:
+       Request Handle
+
+    Output:
+       Request Handle updated with the results.
+       Return code - 0: ok, non-zero: error
+    """
+
+    showInvLines(rh)
+    showOperandLines(rh)
+    return 0
+
+
+def parseCmdline(rh):
+    """
+    Parse the request command input.
+
+    Input:
+       Request Handle
+
+    Output:
+       Request Handle updated with parsed input.
+       Return code - 0: ok, non-zero: error
+    """
+
+    rc = 0
+    rh.printSysLog("Enter deleteVM.parseCmdline")
+
+    if rh.totalParms >= 2:
+        rh.userid = rh.request[1].upper()
+    else:
+        rh.printLn("ES", "Userid is missing")
+        rh.updateResults({'overallRC': 1})
+        rh.printSysLog("Exit deleteVM.parseCmdLine, rc: " + rc)
+        return 1
+
+    if rh.totalParms == 2:
+        rh.subfunction = rh.userid
+        rh.userid = ''
+
+    if rh.totalParms >= 3:
+        rh.subfunction = rh.request[2].upper()
+
+    # Verify the subfunction is valid.
+    if rh.subfunction not in subfuncHandler:
+        list = ', '.join(sorted(subfuncHandler.keys()))
+        rh.printLn("ES", "Subfunction is missing.  " +
+                "It should be one of the following: " + list + ".")
+        rh.updateResults({'overallRC': 4})
+        rc = 4
+
+    # Parse the rest of the command line.
+    if rc == 0:
+        rh.argPos = 3               # Begin Parsing at 4th operand
+        rc = generalUtils.parseCmdline(rh, posOpsList, keyOpsList)
+
+    rh.printSysLog("Exit deleteVM.parseCmdLine, rc: " + str(rc))
+    return rc
+
+
+def showInvLines(rh):
+    """
+    Produce help output related to command synopsis
+
+    Input:
+       Request Handle
+    """
+
+    if rh.subfunction != '':
+        rh.printLn("N", "Usage:")
+    rh.printLn("N", "  python " + rh.cmdName +
+        " DeleteVM <userid> directory")
+    rh.printLn("N", "  python " + rh.cmdName + " DeleteVM help")
+    rh.printLn("N", "  python " + rh.cmdName +
+        " DeleteVM version")
+    return
+
+
+def showOperandLines(rh):
+    """
+    Produce help output related to operands.
+
+    Input:
+       Request Handle
+    """
+
+    if rh.function == 'HELP':
+        rh.printLn("N", "  For the DeleteVM function:")
+    else:
+        rh.printLn("N", "Sub-Functions(s):")
+    rh.printLn("N", "      directory     - " +
+        "Delete a virtual machine from the user directory.")
+    rh.printLn("N", "      help          - " +
+        "Displays this help information.")
+    rh.printLn("N", "      version       - " +
+        "Show the version of the power function")
+    if rh.subfunction != '':
+        rh.printLn("N", "Operand(s):")
+    rh.printLn("N", "      <userid>      - " +
+        "Userid of the target virtual machine")
+
+    return
---
+++ smutLayer/generalUtils.py
@@ -0,0 +1,308 @@
+# General Utilities for Systems Management Ultra Thin Layer
+#
+# Copyright 2017 IBM Corp.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import math
+
+fiveGigSize = (1024 * 5)
+
+
+def cvtToBlocks(rh, diskSize):
+    """
+    Convert a disk storage value to a number of blocks.
+
+    Input:
+       Request Handle
+       Size of disk in bytes
+
+    Output:
+       Results structure:
+          overallRC - Overall return code for the function:
+                      0  - Everything went ok
+                      4  - Input validation error
+          rc        - Return code causing the return. Same as overallRC.
+          rs        - Reason code causing the return.
+          errno     - Errno value causing the return. Always zero.
+       Converted value in blocks
+    """
+
+    rh.printSysLog("Enter generalUtils.cvtToBlocks")
+    blocks = 0
+    results = {'overallRC': 0, 'rc': 0, 'rs': 0, 'errno': 0}
+
+    blocks = diskSize.strip().upper()
+    lastChar = blocks[-1]
+    if lastChar == 'G' or lastChar == 'M':
+        # Convert the bytes to blocks
+        byteSize = blocks[:-1]
+        if byteSize == '':
+            rh.printLn("ES", "The size of the disk is not valid: " + lastChar)
+            results['overallRC'] = 4
+            results['rc'] = 4
+            results['rs'] = 4
+        else:
+            try:
+                if lastChar == 'M':
+                    blocks = (float(byteSize) * 1024 * 1024) / 512
+                elif lastChar == 'G':
+                    blocks = (float(byteSize) * 1024 * 1024 * 1024) / 512
+                blocks = str(int(math.ceil(blocks)))
+            except:
+                rh.printLn("ES", "Failed to convert " + diskSize +
+                    " to a number of blocks")
+                results['overallRC'] = 4
+                results['rc'] = 4
+                results['rs'] = 8
+    elif blocks.strip('1234567890'):
+        rh.printLn("ES", rh.parms['diskSize'] + " is not an integer size " +
+            "of blocks.")
+        results['overallRC'] = 4
+        results['rc'] = 4
+        results['rs'] = 12
+
+    rh.printSysLog("Exit generalUtils.cvtToBlocks, rc: " +
+        str(results['overallRC']))
+    return results, blocks
+
+
+def cvtToCyl(rh, diskSize):
+    """
+    Convert a disk storage value to a number of cylinders.
+
+    Input:
+       Request Handle
+       Size of disk in bytes
+
+    Output:
+       Results structure:
+          overallRC - Overall return code for the function:
+                      0  - Everything went ok
+                      4  - Input validation error
+          rc        - Return code causing the return. Same as overallRC.
+          rs        - Reason code causing the return.
+          errno     - Errno value causing the return. Always zero.
+       Converted value in cylinders
+    """
+
+    rh.printSysLog("Enter generalUtils.cvtToCyl")
+    cyl = 0
+    results = {'overallRC': 0, 'rc': 0, 'rs': 0, 'errno': 0}
+
+    cyl = diskSize.strip().upper()
+    lastChar = cyl[-1]
+    if lastChar == 'G' or lastChar == 'M':
+        # Convert the bytes to cylinders
+        byteSize = cyl[:-1]
+        if byteSize == '':
+            rh.printLn("ES", "The size of the disk is not valid: " + lastChar)
+            results['overallRC'] = 4
+            results['rc'] = 4
+            results['rs'] = 4
+        else:
+            try:
+                if lastChar == 'M':
+                    cyl = (float(byteSize) * 1024 * 1024) / 737280
+                elif lastChar == 'G':
+                    cyl = (float(byteSize) * 1024 * 1024 * 1024) / 737280
+                cyl = str(int(math.ceil(cyl)))
+            except:
+                rh.printLn("ES", "Failed to convert " + diskSize +
+                    " to a number of cylinders")
+                results['overallRC'] = 4
+                results['rc'] = 4
+                results['rs'] = 8
+    elif cyl.strip('1234567890'):
+        rh.printLn("ES", rh.parms['diskSize'] + " is not an integer size " +
+            "of cylinders.")
+        results['overallRC'] = 4
+        results['rc'] = 4
+        results['rs'] = 12
+
+    rh.printSysLog("Exit generalUtils.cvtToCyl, rc: " +
+        str(results['overallRC']))
+    return results, cyl
+
+
+def cvtToMag(rh, size):
+    """
+    Convert a size value to a number with a magnitude appended.
+
+    Input:
+       Request Handle
+       Size bytes
+
+    Output:
+       Results structure:
+          overallRC - Overall return code for the function:
+                      0  - Everything went ok
+                      4  - Input validation error
+          rc        - Return code causing the return. Same as overallRC.
+          rs        - Reason code causing the return.
+          errno     - Errno value causing the return. Always zero.
+       Converted value with a magnitude
+    """
+
+    rh.printSysLog("Enter generalUtils.cvtToMag")
+
+    mSize = ''
+    size = size / (1024 * 1024)
+
+    if size > (1024 * 5):
+        # Size is greater than 5G. Using "G" magnitude.
+        size = size / 1024
+        mSize = "%.1fG" % size
+    else:
+        # Size is less than or equal 5G. Using "M" magnitude.
+        mSize = "%.1fM" % size
+
+    rh.printSysLog("Exit generalUtils.cvtToMag, magSize: " + mSize)
+    return mSize
+
+
+def parseCmdline(rh, posOpsList, keyOpsList):
+    """
+    Parse the request command input.
+
+    Input:
+       Request Handle
+
+    Output:
+       Request Handle updated with parsed input.
+       Return code - 0: ok, non-zero: error
+    """
+
+    rc = 0
+    rh.printSysLog("Enter generalUtils.parseCmdline")
+
+    # Handle any positional operands on the line.
+    if rc == 0 and rh.subfunction in posOpsList:
+        ops = posOpsList[rh.subfunction]
+        currOp = 0
+        while rh.argPos < rh.totalParms and currOp < len(ops):
+            key = ops[currOp][1]
+            opType = ops[currOp][3]
+            if opType == 1:
+                try:
+                    rh.parms[key] = int (rh.request[rh.argPos])
+                except ValueError:
+                    # keyword is not an integer
+                    rh.printLn("ES", rh.function + " " + rh.subfunction +
+                        " subfunction's operand at position " + str(currOp+1) +
+                        " (" + ops[currOp][0] +
+                        ") is not an integer: " + rh.request[rh.argPos])
+                    rh.updateResults({'overallRC': 4})
+                    rc = 4
+                    break
+            else:
+                rh.parms[key] = rh.request[rh.argPos]
+            currOp += 1
+            rh.argPos += 1
+
+        if (rh.argPos >= rh.totalParms and currOp < len(ops) and
+            ops[currOp][2] is True):
+            # Check for missing required operands.
+            rh.printLn("ES", rh.function + "'s " + rh.subfunction +
+               " subfunction is missing positional operand number " +
+               " (" + ops[currOp][0] + " operand).")
+            rh.updateResults({'overallRC': 4})
+            rc = 4
+
+    # Handle any keyword operands on the line.
+    if rc == 0 and rh.subfunction in keyOpsList:
+        while rh.argPos < rh.totalParms:
+            if rh.request[rh.argPos] in keyOpsList[rh.subfunction]:
+                keyword = rh.request[rh.argPos]
+                rh.argPos += 1
+                ops = keyOpsList[rh.subfunction]
+                if keyword in ops:
+                    key = ops[keyword][0]
+                    opCnt = ops[keyword][1]
+                    opType = ops[keyword][2]
+                    if opCnt == 0:
+                        # Keyword has no additional value
+                        rh.parms[key] = True
+                    else:
+                        # Keyword has values following it.
+                        if opCnt < 0:
+                            # Property is a list all of the rest of the parms.
+                            opCnt = rh.totalParms - rh.argPos
+                            if opCnt == 0:
+                                # Need at least 1 operand value
+                                opCnt = 1
+                        if opCnt + rh.argPos > rh.totalParms:
+                            # keyword is missing its related value operand
+                            rh.printLn("ES", rh.function + " " +
+                                rh.subfunction + " subfunction's " +
+                                keyword + " operand is missing a value")
+                            rh.updateResults({'overallRC': 4})
+                            rc = 4
+                            break
+
+                        """
+                        Add the expected value to the property.
+                        Take into account if there are more than 1.
+                        """
+                        if opCnt > 1:
+                            # Initialize the list.
+                            rh.parms[key] = []
+                        for i in range(0, opCnt):
+                            #print "Value: " + rh.request[rh.argPos]
+                            if opType == 1:
+                                # convert from string to int and save it.
+                                try:
+                                    if opCnt == 1:
+                                        rh.parms[key] = (
+                                            int(rh.request[rh.argPos]))
+                                    else:
+                                        rh.parms[key].append(int(
+                                            rh.request[rh.argPos]))
+                                except ValueError:
+                                    # keyword is not an integer
+                                    rh.printLn("ES", rh.function + " " +
+                                        rh.subfunction + " subfunction's " +
+                                        keyword + " operand value is not an " +
+                                        "integer: " + rh.request[rh.argPos])
+                                    rh.updateResults({'overallRC': 4})
+                                    rc = 4
+                                    break
+                            else:
+                                # Value is a string, save it.
+                                if opCnt == 1:
+                                    rh.parms[key] = rh.request[rh.argPos]
+                                else:
+                                    rh.parms[key].append(rh.request[rh.argPos])
+                            rh.argPos += 1
+                        if rc != 0:
+                            # Upper loop had an error break from loops.
+                            break
+                else:
+                    # keyword is not in the subfunction's keyword list
+                    rh.printLn("ES", rh.function + "'s subfunction " +
+                        rh.subfunction + " does not recognize: " +
+                        rh.request[rh.argPos])
+                    rh.updateResults({'overallRC': 4})
+                    rc = 4
+                    break
+            else:
+                # Subfunction does not support keywords
+                rh.printLn("ES", rh.function + "'s subfunction " +
+                    rh.subfunction + " does not recognize: " +
+                    rh.request[rh.argPos])
+                rh.updateResults({'overallRC': 4})
+                rc = 4
+                break
+
+    rh.printSysLog("Exit generalUtils.parseCmdLine, rc: " + str(rc))
+    return rc
