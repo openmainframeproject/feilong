51441ef690e1ef9e390f04bdf605555f83c1dba2
dongyan yang <dyyang@cn.ibm.com>
2017-07-17 17:11:34 +0800

    FVT of vnic: couple/uncouple/delete, guest_get_nic_vswitch_info
    Change-Id: I24246991eb2db8050653b3d08dbb068d4aa8e72c

--- zvmsdk/tests/functional/test_networkops.py
+++ zvmsdk/tests/functional/test_networkops.py
@@ -264,6 +264,28 @@ class SDKNICTestCase(base.SDKAPIBaseTestCase):
         self.sdkapi.guest_create(self.basevm, 1, 1024)
         self.addCleanup(self.sdkapi.guest_delete, self.basevm)
 
+    def _activate_vm(self, userid):
+        """ Add IPL entry to VM and then Power on the VM. """
+        zhcp_node = self.client._get_hcp_info()['nodename']
+        cmd = ' '.join((
+            '/opt/zhcp/bin/smcli Image_Definition_Update_DM',
+            "-T %s" % userid,
+            "-k 'IPL=VDEV=190'",
+            "-k 'LINK=USERID=MAINT VDEV1=0190 MODE=RR'"))
+        self.client.run_commands_on_node(zhcp_node, cmd)
+        print("Powering on the guest.")
+        # Try 5 times to power on the vm.
+        for i in range(5):
+            try:
+                self.sdkapi.guest_start(userid)
+            except exception.ZVMXCATInternalError as err:
+                err_str = err.format_message()
+                if ("Return Code: 396" in err_str and
+                        "Reason Code: 59" in err_str):
+                    continue
+                else:
+                    raise
+
     def _check_nic(self, vdev, mac=None, vsw=None, devices=3):
         """ Check nic status.
         Returns a bool value to indicate whether the nic is defined in user and
@@ -294,7 +316,6 @@ class SDKNICTestCase(base.SDKAPIBaseTestCase):
 
     def test_guest_create_nic(self):
         """ Test each parameter of guest_create_nic. """
-        zhcp_node = self.client._get_hcp_info()['nodename']
 
         print("Creating NIC with default vdev, not active.")
         self.sdkapi.guest_create_nic(self.basevm, persist=True)
@@ -379,25 +400,9 @@ class SDKNICTestCase(base.SDKAPIBaseTestCase):
                           self.basevm, vdev='7006',
                           active=True)
 
-        print("Updating guest definition so that it can be started.")
-        cmd = ' '.join((
-            '/opt/zhcp/bin/smcli Image_Definition_Update_DM',
-            "-T %s" % self.basevm,
-            "-k 'IPL=VDEV=190'",
-            "-k 'LINK=USERID=MAINT VDEV1=0190 MODE=RR'"))
-        self.client.run_commands_on_node(zhcp_node, cmd)
-        print("Powering on the guest.")
-        # Try 5 times to power on the vm.
-        for i in range(5):
-            try:
-                self.sdkapi.guest_start(self.basevm)
-            except exception.ZVMXCATInternalError as err:
-                err_str = err.format_message()
-                if ("Return Code: 396" in err_str and
-                        "Reason Code: 59" in err_str):
-                    continue
-                else:
-                    raise
+        # Activate the VM
+        print("Activating the VM.")
+        self._activate_vm(self.basevm)
         print("Creating nic to the active guest.")
         self.sdkapi.guest_create_nic(self.basevm, vdev='8000',
                                      active=True, persist=True)
@@ -405,3 +410,152 @@ class SDKNICTestCase(base.SDKAPIBaseTestCase):
         nic_defined, vsw = self._check_nic('8000')
         self.assertTrue(nic_defined)
         self.assertEqual(vsw, "")
+
+    def test_nic_couple_uncouple_delete(self):
+        """ Test couple/uncouple/delete NIC. """
+        vswitch = self.vswitch
+        vswitch2 = 'VSWTEST2'
+        vm = self.basevm
+        self.sdkapi.vswitch_create(vswitch)
+        self.sdkapi.vswitch_create(vswitch2)
+        self.addCleanup(self.sdkapi.vswitch_delete, vswitch)
+        self.addCleanup(self.sdkapi.vswitch_delete, vswitch2)
+
+        print("Couple/uncouple/delete, inactive mode.")
+        print("Creating NIC with fixed vdev.")
+        self.sdkapi.guest_create_nic(vm, vdev='1000')
+        # Check nic defined in user direct and not coupled to vswitch
+        nic_defined, vsw = self._check_nic('1000')
+        self.assertTrue(nic_defined)
+        self.assertEqual(vsw, "")
+
+        print("Coupling with active=True on an off-state VM.")
+        # The active should fail and rollback the user direct and switch table
+        self.assertRaises(exception.ZVMException,
+                          self.sdkapi.guest_nic_couple_to_vswitch,
+                          vm, '1000', vswitch, active=True)
+        nic_defined, vsw = self._check_nic('1000')
+        self.assertTrue(nic_defined)
+        self.assertEqual(vsw, '')
+
+        print("Coupling NIC to VSWITCH.")
+        self.sdkapi.guest_nic_couple_to_vswitch(vm, '1000', vswitch)
+        nic_defined, vsw = self._check_nic('1000', vsw=vswitch)
+        self.assertTrue(nic_defined)
+        self.assertEqual(vsw, vswitch)
+
+        print("Coupling NIC to VSWITCH second time, same vswitch, supported.")
+        self.sdkapi.guest_nic_couple_to_vswitch(vm, '1000', vswitch)
+        nic_defined, vsw = self._check_nic('1000', vsw=vswitch)
+        self.assertTrue(nic_defined)
+        self.assertEqual(vsw, vswitch)
+
+        print("Coupling NIC to VSWITCH, to different vswitch, not supported.")
+        # Should still be coupled to original vswitch
+        self.assertRaises(exception.ZVMException,
+                          self.sdkapi.guest_nic_couple_to_vswitch,
+                          vm, '1000', vswitch2)
+        nic_defined, vsw = self._check_nic('1000', vsw=vswitch)
+        self.assertTrue(nic_defined)
+        self.assertEqual(vsw, vswitch)
+
+        print("Uncoupling with active=True on an off-state VM.")
+        self.assertRaises(exception.ZVMException,
+                          self.sdkapi.guest_nic_uncouple_from_vswitch,
+                          vm, '1000', active=True)
+        # The NIC shoule be uncoupled in user direct and switch table
+        nic_defined, vsw = self._check_nic('1000')
+        self.assertTrue(nic_defined)
+        self.assertEqual(vsw, "")
+
+        print("Uncoupling NIC from VSWITCH the second time.")
+        self.sdkapi.guest_nic_uncouple_from_vswitch(vm, '1000')
+        nic_defined, vsw = self._check_nic('1000')
+        self.assertTrue(nic_defined)
+        self.assertEqual(vsw, "")
+
+        print("Deleting NIC.")
+        self.sdkapi.guest_delete_nic(vm, '1000')
+        nic_defined, vsw = self._check_nic('1000')
+        self.assertNotTrue(nic_defined)
+
+        print("Deleting NIC not existed.")
+        self.sdkapi.guest_delete_nic(vm, '1000')
+
+        print("Positive case of Couple/uncouple/delete, active mode.")
+        print("Activating the VM.")
+        self._activate_vm(vm)
+        print("Creating NIC with fixed vdev.")
+        self.sdkapi.guest_create_nic(vm, vdev='1000', active=True)
+        # Check nic defined in user direct and not coupled to vswitch
+        nic_defined, vsw = self._check_nic('1000')
+        self.assertTrue(nic_defined)
+        self.assertEqual(vsw, "")
+
+        print("Unauthorized to couple NIC in active mode.")
+        self.assertRaises(exception.ZVMException,
+                          self.sdkapi.guest_nic_couple_to_vswitch,
+                          vm, '1000', vswitch, active=True)
+        nic_defined, vsw = self._check_nic('1000')
+        self.assertTrue(nic_defined)
+        self.assertEqual(vsw, "")
+
+        print("Authorizing VM to couple to vswitch.")
+        self.sdkapi.vswitch_grant_user(vswitch, vm)
+
+        print("Coupling NIC to an unexisted vswitch.")
+        # active should rollback user direct and raise exception
+        if "VSWNONE" in self.sdkapi.vswitch_get_list():
+            self.sdkapi.vswitch_delete("SDKNONE")
+        self.assertRaises(exception.ZVMException,
+                          self.sdkapi.guest_nic_couple_to_vswitch,
+                          vm, '1000', "VSWNONE", active=True)
+        nic_defined, vsw = self._check_nic('1000')
+        self.assertTrue(nic_defined)
+        self.assertEqual(vsw, "")
+
+        print("Coupling NIC to VSWITCH.")
+        self.sdkapi.guest_nic_couple_to_vswitch(vm, '1000', vswitch,
+                                                active=True)
+        nic_defined, vsw = self._check_nic('1000', vsw=vswitch)
+        self.assertTrue(nic_defined)
+        self.assertEqual(vsw, vswitch)
+
+        print("Coupling NIC to VSWITCH second time, same vswitch, supported.")
+        self.sdkapi.guest_nic_couple_to_vswitch(vm, '1000', vswitch,
+                                                active=True)
+        nic_defined, vsw = self._check_nic('1000', vsw=vswitch)
+        self.assertTrue(nic_defined)
+        self.assertEqual(vsw, vswitch)
+
+        print("Coupling NIC to VSWITCH, to different vswitch, not supported.")
+        # Should still be coupled to original vswitch
+        self.sdkapi.vswitch_grant_user(vswitch2, vm)
+        self.assertRaises(exception.ZVMException,
+                          self.sdkapi.guest_nic_couple_to_vswitch,
+                          vm, '1000', vswitch2, active=True)
+        nic_defined, vsw = self._check_nic('1000', vsw=vswitch)
+        self.assertTrue(nic_defined)
+        self.assertEqual(vsw, vswitch)
+
+        print("Uncoupling NIC from VSWITCH.")
+        self.sdkapi.guest_nic_uncouple_from_vswitch(vm, '1000', active=True)
+        nic_defined, vsw = self._check_nic('1000')
+        self.assertTrue(nic_defined)
+        self.assertEqual(vsw, "")
+
+        print("Uncoupling NIC from VSWITCH the second time.")
+        self.sdkapi.guest_nic_uncouple_from_vswitch(vm, '1000', active=True)
+        nic_defined, vsw = self._check_nic('1000')
+        self.assertTrue(nic_defined)
+        self.assertEqual(vsw, "")
+
+        print("Deleting NIC.")
+        self.sdkapi.guest_delete_nic(vm, '1000', active=True)
+        nic_defined, vsw = self._check_nic('1000')
+        self.assertNotTrue(nic_defined)
+
+        print("Deleting NIC not existed.")
+        self.sdkapi.guest_delete_nic(vm, '1000', active=True)
+        nic_defined, vsw = self._check_nic('1000')
+        self.assertNotTrue(nic_defined)
