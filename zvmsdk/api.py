ddfc962459b026c3e96b7316a21f62ce656ee6b7
PanYaLian <ylpan@cn.ibm.com>
2017-07-24 15:59:39 +0800

    Document exceptions for API guest_create_nic
    change-Id: I109564d997733fabb764b526ff03b5b8cef2181b

--- zvmsdk/api.py
+++ zvmsdk/api.py
@@ -306,7 +306,13 @@ class SDKAPI(object):
         :param str ip_addr: the management IP address of the guest, it should
                be the value between 0.0.0.0-255.255.255.255
         :param bool active: whether add a nic on active guest system
-
+        :raises ZVMInvalidInput
+                - Invalid mac address or IP address is provided.
+                - The specified virtual device number has already been used.
+        :raises ZVMNetworkError
+                - The virtual device number is out of the range.
+                - All kinds of xCAT call failure and reraise.
+                - Smcli call failure, refer to the error message for detail.
         """
         if mac_addr is not None:
             if not utils.valid_mac_addr(mac_addr):
