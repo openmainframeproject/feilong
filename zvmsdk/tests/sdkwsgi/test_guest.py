b39d86738ca40b410529b5ce2db056f9b5664e3b
jichenjc <jichenjc@cn.ibm.com>
2017-07-29 04:10:10 +0800

    verify nic info
    Change-Id: If9a5d986daa8497ad95b14e485d6ac7357e6c918

--- zvmsdk/tests/sdkwsgi/test_guest.py
+++ zvmsdk/tests/sdkwsgi/test_guest.py
@@ -65,11 +65,19 @@ class GuestHandlerTestCase(unittest.TestCase):
 
     def _guest_nic_delete(self, vdev="1000"):
         body = '{"nic": {}}'
-        url = '/guests/%s/nic/2000' % self.userid
+        url = '/guests/%s/nic/%s' % (self.userid, vdev)
         resp = self.client.api_request(url=url,
                                        method='DELETE',
                                        body=body)
         self.assertEqual(200, resp.status_code)
+
+    def _guest_nic_query(self):
+        url = '/guests/%s/nic' % self.userid
+        resp = self.client.api_request(url=url,
+                                       method='GET')
+        self.assertEqual(200, resp.status_code)
+        self.apibase.verify_result('test_guest_get_nic', resp.content)
+        return resp
 
     def _guest_get(self):
         url = '/guests/%s' % self.userid
@@ -230,6 +238,8 @@ class GuestHandlerTestCase(unittest.TestCase):
             self._vswitch_couple()
 
             self._vswitch_uncouple()
+
+            self._guest_nic_query()
 
             self._guest_nic_delete()
 
