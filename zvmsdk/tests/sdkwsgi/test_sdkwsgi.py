3f5d472dd66cb10a17f51668087314232ca11fc4
jichenjc <jichenjc@cn.ibm.com>
2017-07-29 18:14:58 +0800

    use token validation
    Change-Id: I3cc772063401184c706ab89b47a74754bed2bf8f

--- zvmsdk/tests/sdkwsgi/test_sdkwsgi.py
+++ zvmsdk/tests/sdkwsgi/test_sdkwsgi.py
@@ -16,9 +16,19 @@ class TestSDKClient(object):
     def __init__(self):
         self.base_url = "http://127.0.0.1:8888"
 
+    def _get_token(self):
+        url = self.base_url + '/token'
+        method = 'POST'
+        response = self.request(url, method, None)
+        token = response.headers['X-Auth-Token']
+
+        return token
+
     def request(self, url, method, body, headers=None):
         _headers = {'Content-Type': 'application/json'}
         _headers.update(headers or {})
+
+        _headers['X-Auth-Token'] = self._get_token()
 
         response = requests.request(method, url, data=body, headers=_headers)
         return response
