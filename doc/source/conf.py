14616038b89d4ea917a71ba16e2b368fbac98912
jichenjc <jichenjc@cn.ibm.com>
2017-08-19 01:50:25 +0800

    WIP: add doc ext table
    Change-Id: I4c76d2bc8da31c970516962ff0acf3649330c09d

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -27,13 +27,16 @@ import warnings
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
+sys.path.insert(0, os.path.abspath('../../../'))
 sys.path.insert(0, os.path.abspath('../../'))
 sys.path.insert(0, os.path.abspath('../'))
 sys.path.insert(0, os.path.abspath('./'))
 
 extensions = [
     'sphinx.ext.autodoc',
+    'ext.restapi_parameters',
 ]
+
 # -- General configuration ----------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
