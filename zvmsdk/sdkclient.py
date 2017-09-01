a5657a94f5c21df967ed92ee6f76ee76bfcdebb3
dongyan yang <dyyang@cn.ibm.com>
2017-09-01 14:57:47 +0800

    close the socket in finally block for both client and server
    make sure the sockets are closed immediately after use even when there
    is any unexpected exceptions happen.
    
    Change-Id: If6e0dd0491fddaf1fbc3ffde69e4a1a5021d6d6e

--- zvmsdk/sdkclient.py
+++ zvmsdk/sdkclient.py
@@ -43,65 +43,69 @@ class SDKClient(object):
                     'errmsg': 'Failed to create client socket: %s' % msg,
                     'output': ''}
 
-        # Connect SDK server
         try:
-            cs.connect((self.addr, self.port))
-        except socket.error as msg:
-            cs.close()
-            return {'overallRC': 1,
-                    'rc': 1,
-                    'rs': 2,
-                    'errmsg': 'Failed to connect SDK server: %s' % msg,
-                    'output': ''}
+            # Connect SDK server
+            try:
+                cs.connect((self.addr, self.port))
+            except socket.error as msg:
+                return {'overallRC': 1,
+                        'rc': 1,
+                        'rs': 2,
+                        'errmsg': 'Failed to connect SDK server: %s' % msg,
+                        'output': ''}
 
-        # Prepare the data to be sent
-        api_data = json.dumps((func, api_args, api_kwargs))
-        print "generated command: %s" % api_data
-        # Send the API call data to SDK server
-        sent = 0
-        total_len = len(api_data)
-        got_error = False
-        while (sent < total_len):
-            this_sent = cs.send(api_data[sent:])
-            if this_sent == 0:
-                got_error = True
-                break
-            sent += this_sent
-        if got_error or sent != total_len:
-            cs.close()
-            return {'overallRC': 1,
-                    'rc': 1,
-                    'rs': 3,
-                    'errmsg': ('Failed to send API call to SDK server,'
-                               '%d bytes sent. API call: %s') % (sent,
-                                                                 api_data),
-                    'output': ''}
-        else:
-            print "API call is sent to SDK server successfully: %s" % api_data
+            # Prepare the data to be sent
+            api_data = json.dumps((func, api_args, api_kwargs))
+            print "generated command: %s" % api_data
+            # Send the API call data to SDK server
+            sent = 0
+            total_len = len(api_data)
+            got_error = False
+            while (sent < total_len):
+                this_sent = cs.send(api_data[sent:])
+                if this_sent == 0:
+                    got_error = True
+                    break
+                sent += this_sent
+            if got_error or sent != total_len:
+                return {'overallRC': 1,
+                        'rc': 1,
+                        'rs': 3,
+                        'errmsg': ('Failed to send API call to SDK server,'
+                                   '%d bytes sent. API call: %s') % (sent,
+                                                                     api_data),
+                        'output': ''}
+            else:
+                print ("API call is sent to SDK server successfully: %s" %
+                       api_data)
 
-        # Receive data from server
-        return_blocks = []
-        while True:
-            # TODO: Add timeout to socket
-            block = cs.recv(4096)
-            if not block:
-                break
-            return_blocks.append(block)
-        # Close the socket
-        cs.close()
+            # Receive data from server
+            return_blocks = []
+            while True:
+                # TODO: Add timeout to socket, otherwise when SDK server got
+                # uncaught exception or shutdown, the client would be blocked
+                # here forever.
+                block = cs.recv(4096)
+                if not block:
+                    break
+                return_blocks.append(block)
+        finally:
+            # Always close the client socket to avoid too many hanging
+            # socket left.
+            cs.close()
 
         # Transform the received stream to standard result form
         # This client assumes that the server would return result in
         # the standard result form, so client just return the received
         # data
         if return_blocks != []:
-            result = json.loads(''.join(return_blocks))
-            print "Received results from server: %s" % result
+            results = json.loads(''.join(return_blocks))
+            print "Received results from server: %s" % results
         else:
-            result = {'overallRC': 1,
+            results = {'overallRC': 1,
                       'rc': 1,
                       'rs': 4,
                       'errmsg': ('Failed to receive API results from'
                                  'SDK server'),
                       'output': ''}
-        return result
+        return results
