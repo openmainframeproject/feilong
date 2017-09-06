55883109964520f54193f7c01dd06b42e9d39a7e
nafei yang <nafeiy@cn.ibm.com>
2017-09-06 15:59:44 +0800

    Move image operations to smutcliet
    This will make the structure more clearly
    It will not block smut ut this time
    
    Change-Id: Idd2b6f05c2d85435abc22a3cee8e8d891dffe74c

--- zvmsdk/smutclient.py
+++ zvmsdk/smutclient.py
@@ -12,10 +12,16 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-
+import commands
+import hashlib
+import urlparse
+import requests
+import threading
 import os
 import re
+import shutil
 import tempfile
+
 
 from smutLayer import smut
 
@@ -30,6 +36,8 @@ from zvmsdk import utils as zvmutils
 
 CONF = config.CONF
 LOG = log.LOG
+_LOCK = threading.Lock()
+CHUNKSIZE = 4096
 
 
 class SMUTClient(client.ZVMClient):
@@ -39,6 +47,7 @@ class SMUTClient(client.ZVMClient):
         self._smut = smut.SMUT()
         self._NetDbOperator = database.NetworkDbOperator()
         self._GuestDbOperator = database.GuestDbOperator()
+        self._ImageDbOperator = database.ImageDbOperator()
 
     def _request(self, requestData):
         try:
@@ -734,3 +743,251 @@ class SMUTClient(client.ZVMClient):
         LOG.debug("Disconnect nic %s with network %s",
                   nic_vdev, msg)
         self._uncouple_nic(userid, nic_vdev, active=active)
+
+    def image_import(self, image_name, url, image_meta, remote_host=None):
+        """Import the image specified in url to SDK image repository, and
+        create a record in image db, the imported images are located in
+        image_repository/prov_method/os_version/, for example,
+        /opt/sdk/images/netboot/rhel7.2/90685d2b-167b.img"""
+
+        try:
+            # Ensure the specified image is not exisit in image DB
+            image_exists = self._ImageDbOperator.image_query_record(image_name)
+            if image_exists:
+                LOG.info("The image %s has already exist in image repository"
+                         % image_name)
+                return
+            target = self._get_image_path_by_name(image_name,
+                image_meta['os_version'], const.IMAGE_TYPE['DEPLOY'])
+            self._pathutils.create_import_image_repository(
+                image_meta['os_version'], const.IMAGE_TYPE['DEPLOY'])
+            self._scheme2backend(urlparse.urlparse(url).scheme).image_import(
+                                                    image_name, url,
+                                                    image_meta,
+                                                    remote_host=remote_host)
+            # Check md5 after import to ensure import a correct image
+            # TODO change to use query imagename in db
+            expect_md5sum = image_meta.get('md5sum')
+            real_md5sum = self._get_md5sum(target)
+            if expect_md5sum and expect_md5sum != real_md5sum:
+                err = ("The md5sum after import is not same as source image,"
+                       " the image has been broken")
+                raise exception.SDKImageImportException(err)
+            LOG.info("Image %s is import successfully" % image_name)
+            disk_size_units = self._get_disk_size_units(target)
+            image_size = self._get_image_size(target)
+            self._ImageDbOperator.image_add_record(image_name,
+                                         image_meta['os_version'],
+                                         real_md5sum,
+                                         disk_size_units,
+                                         image_size,
+                                         const.IMAGE_TYPE['DEPLOY'])
+        except KeyError:
+            raise exception.SDKUnsupportedImageBackend("No backend found for"
+                        " '%s'" % urlparse.urlparse(url).scheme)
+        except Exception as err:
+            msg = ("Import image to zvmsdk image repository error due to: %s"
+                   % str(err))
+            # Cleanup the image from image repository
+            self._pathutils.remove_file(target)
+            raise exception.SDKImageImportException(msg=msg)
+
+    def _get_disk_size_units(self, image_path):
+        """Return a string to indicate disk units in format 3390:CYL or 408200:
+        BLK"""
+        command = 'hexdump -n 48 -C %s' % image_path
+        (rc, output) = commands.getstatusoutput(command)
+        LOG.debug("hexdump result is %s" % output)
+        if rc:
+            msg = ("Error happened when executing command hexdump with"
+                   "reason: %s" % output)
+            LOG.error(msg)
+            raise exception.ZVMImageError(msg=msg)
+
+        try:
+            root_disk_size = int(output[144:156])
+            disk_units = output[220:223]
+            root_disk_units = ':'.join([str(root_disk_size), disk_units])
+        except ValueError:
+            msg = ("Image file at %s is missing built-in disk size "
+                   "metadata, it was probably not captured by SDK" %
+                   image_path)
+            raise exception.ZVMImageError(msg=msg)
+
+        if 'FBA' not in output and 'CKD' not in output:
+            msg = ("The image's disk type is not valid. Currently we only"
+                      " support FBA and CKD disk")
+            raise exception.ZVMImageError(msg=msg)
+
+        LOG.debug("The image's root_disk_units is %s" % root_disk_units)
+        return root_disk_units
+
+    def _get_image_size(self, image_path):
+        """Return disk size in bytes"""
+        command = 'du -b %s' % image_path
+        (rc, output) = commands.getstatusoutput(command)
+        if rc:
+            msg = ("Error happened when executing command du -b with"
+                   "reason: %s" % output)
+            LOG.error(msg)
+            raise exception.ZVMImageError(msg=msg)
+        size = output.split()[0]
+        return size
+
+    def _get_image_path_by_name(self, image_name, image_os_version, type):
+        target = '/'.join([CONF.image.sdk_image_repository,
+                           type,
+                           image_os_version,
+                           image_name])
+        return target
+
+    def _scheme2backend(self, scheme):
+        return {
+            "file": FilesystemBackend,
+            "http": HTTPBackend,
+    #         "https": HTTPSBackend
+        }[scheme]
+
+    def _get_md5sum(self, fpath):
+        """Calculate the md5sum of the specific image file"""
+        current_md5 = hashlib.md5()
+        if isinstance(fpath, basestring) and os.path.exists(fpath):
+            with open(fpath, "rb") as fh:
+                for chunk in self._read_chunks(fh):
+                    current_md5.update(chunk)
+
+        elif (fpath.__class__.__name__ in ["StringIO", "StringO"] or
+              isinstance(fpath, file)):
+            for chunk in self._read_chunks(fpath):
+                current_md5.update(chunk)
+        else:
+            return ""
+        return current_md5.hexdigest()
+
+    def _read_chunks(self, fh):
+        fh.seek(0)
+        chunk = fh.read(CHUNKSIZE)
+        while chunk:
+            yield chunk
+            chunk = fh.read(CHUNKSIZE)
+        else:
+            fh.seek(0)
+
+    def image_delete(self, image_name):
+        # Delete image file
+        self._delete_image_file(image_name)
+        # Delete image record from db
+        self._ImageDbOperator.image_delete_record(image_name)
+
+    def _delete_image_file(self, image_name):
+        target_info = self._ImageDbOperator.image_query_record(image_name)
+        image_path = '/'.join([CONF.image.sdk_image_repository,
+                               target_info[5],
+                               target_info[1],
+                               image_name])
+
+        self._pathutils.remove_file(image_path)
+
+
+class FilesystemBackend(object):
+    @classmethod
+    def image_import(cls, image_name, url, image_meta, **kwargs):
+        """Import image from remote host to local image repository using scp.
+        If remote_host not specified, it means the source file exist in local
+        file system, just copy the image to image repository
+        """
+        try:
+            source = urlparse.urlparse(url).path
+            target = '/'.join([CONF.image.sdk_image_repository,
+                              const.IMAGE_TYPE['DEPLOY'],
+                              image_meta['os_version'],
+                              image_name])
+            if kwargs['remote_host']:
+                if '@' in kwargs['remote_host']:
+                    source_path = ':'.join([kwargs['remote_host'], source])
+                    command = ' '.join(['/usr/bin/scp', source_path, target])
+                    (rc, output) = commands.getstatusoutput(command)
+                    if rc:
+                        msg = ("Error happened when copying image file with"
+                               "reason: %s" % output)
+                        LOG.error(msg)
+                        raise
+                else:
+                    msg = ("The specified remote_host %s format invalid" %
+                            kwargs['remote_host'])
+                    LOG.error(msg)
+                    raise
+            else:
+                LOG.debug("Remote_host not specified, will copy from local")
+                shutil.copyfile(source, target)
+
+        except Exception as err:
+                msg = ("Error happened when importing image to SDK"
+                          " image repository with reason: %s" % str(err))
+                LOG.error(msg)
+                raise err
+
+
+class HTTPBackend(object):
+    @classmethod
+    def image_import(cls, image_name, url, image_meta, **kwargs):
+        import_image = MultiThreadDownloader(image_name, url, image_meta)
+        import_image.run()
+
+
+class MultiThreadDownloader(threading.Thread):
+
+    def __init__(self, image_name, url, image_meta):
+        super(MultiThreadDownloader, self).__init__()
+        self.url = url
+        # Set thread number
+        self.threadnum = 8
+        self.name = image_name
+        self.image_osdistro = image_meta['os_version']
+        r = requests.head(self.url)
+        # Get the size of the download resource
+        self.totalsize = int(r.headers['Content-Length'])
+        self.target = '/'.join([CONF.image.sdk_image_repository, 'netboot',
+                                self.image_osdistro,
+                                self.name])
+
+    def get_range(self):
+        ranges = []
+        offset = int(self.totalsize / self.threadnum)
+        for i in range(self.threadnum):
+            if i == self.threadnum - 1:
+                ranges.append((i * offset, ''))
+            else:
+                # Get the process range for each thread
+                ranges.append((i * offset, (i + 1) * offset))
+        return ranges
+
+    def download(self, start, end):
+        headers = {'Range': 'Bytes=%s-%s' % (start, end),
+                   'Accept-Encoding': '*'}
+        # Get the data
+        res = requests.get(self.url, headers=headers)
+        # seek to the right position for writing data
+        LOG.debug("Downloading file range %s:%s success" % (start, end))
+
+        with _LOCK:
+            self.fd.seek(start)
+            self.fd.write(res.content)
+
+    def run(self):
+        self.fd = open(self.target, 'w')
+        thread_list = []
+        n = 0
+        for ran in self.get_range():
+            start, end = ran
+            LOG.debug('thread %d start:%s,end:%s' % (n, start, end))
+            n += 1
+            # Open thread
+            thread = threading.Thread(target=self.download, args=(start, end))
+            thread.start()
+            thread_list.append(thread)
+
+        for i in thread_list:
+            i.join()
+        LOG.debug('Download %s success' % (self.name))
+        self.fd.close()
