91b69e235aec12590ea1d7583d20c632e98d2915
PanYaLian <ylpan@cn.ibm.com>
2017-09-28 08:50:13 +0800

    Func name error switch_updat_record_with_switch
    Change-Id: Iec305fcbd081e1aebb3b1898076b22b0473f4a06

--- zvmsdk/tests/unit/test_database.py
+++ zvmsdk/tests/unit/test_database.py
@@ -79,17 +79,17 @@ class NetworkDbOperatorTestCase(base.SDKTestCase):
 
     @mock.patch.object(database.NetworkDbOperator,
                        '_get_switch_by_user_interface')
-    def test_switch_updat_record_with_switch_fail(self, get_record):
+    def test_switch_update_record_with_switch_fail(self, get_record):
         get_record.return_value = None
         userid = 'testuser'
         interface = '1000'
         switch = 'testswitch'
 
         self.assertRaises(exception.SDKObjectNotExistError,
-                          self.db_op.switch_updat_record_with_switch,
+                          self.db_op.switch_update_record_with_switch,
                           userid, interface, switch)
 
-    def test_switch_updat_record_with_switch(self):
+    def test_switch_update_record_with_switch(self):
         userid = 'testuser'
         interface = '1000'
         port = 'testport'
@@ -99,7 +99,7 @@ class NetworkDbOperatorTestCase(base.SDKTestCase):
         self.db_op.switch_add_record_for_nic(userid, interface, port)
 
         # update record with switch info
-        self.db_op.switch_updat_record_with_switch(userid, interface, switch)
+        self.db_op.switch_update_record_with_switch(userid, interface, switch)
 
         # query
         switch_record = self.db_op.switch_select_table()
@@ -108,7 +108,7 @@ class NetworkDbOperatorTestCase(base.SDKTestCase):
 
         switch = None
         # update record to remove switch info
-        self.db_op.switch_updat_record_with_switch(userid, interface, switch)
+        self.db_op.switch_update_record_with_switch(userid, interface, switch)
 
         # query
         switch_record = self.db_op.switch_select_table()
