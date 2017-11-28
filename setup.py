3dd68a5b077e9bdbbef851b285ba3f3c4e8667af
jichenjc <jichenjc@cn.ibm.com>
2017-11-28 15:33:06 +0800

    add build rpm
    Change-Id: Id1e4c7d7b207712cb1d5b207842b48c0aa6bdb3b

--- setup.py
+++ setup.py
@@ -44,5 +44,6 @@ setuptools.setup(
     scripts=['scripts/sdkserver', 'scripts/zvmsdk-wsgi'],
     data_files=[('/usr/lib/systemd/system', ['data/sdkserver.service']),
                 ('/var/lib/zvmsdk', ['data/setupDisk']),
+                ('/etc/zvmsdk', ['data/uwsgi-zvmsdk.conf']),
                 ('/etc/zvmsdk', ['doc/source/zvmsdk.conf.sample'])],
 )
