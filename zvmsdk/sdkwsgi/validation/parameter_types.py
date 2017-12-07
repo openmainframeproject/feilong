2d16be40d676d961773d0a71ecc17f1bd7313445
XiaoMeiZheng <xmzheng@cn.ibm.com>
2017-12-07 14:22:40 +0800

    Add qurey param check including get_definition_info,guest_get_info, and guest_get_nic_vswitch_info,guest_get_power_state and guest_inspect_stats,guest_inspect_vnics and image_query,image_get_root_disk_size and host_diskpool_get_info. Change-Id: I77c394259a0d67382e9b05d08899344b96daa488

--- zvmsdk/sdkwsgi/validation/parameter_types.py
+++ zvmsdk/sdkwsgi/validation/parameter_types.py
@@ -219,7 +219,10 @@ remotehost = {
 
 
 userid = {
-    'type': ['string'], 'minLength': 1, 'maxLength': 8
+    'type': ['string'],
+    'minLength': 1,
+    'maxLength': 8,
+    'pattern': '^(\w{,8})$'
 }
 
 
@@ -254,12 +257,26 @@ userid_list = {
     'pattern': '^(\w{,8})(,\w{,8}){0,}$'
 }
 
+userid_list_array = {
+    'items': {
+        'type': ['string'],
+        'minLength': 1,
+        'maxLength': 8,
+        'pattern': '^(\w{,8})(,\w{,8}){0,}$'
+
+    },
+    'type': 'array'
+}
 
 file_type = {
     'type': 'string',
     'enum': ['ext2', 'ext3', 'ext4', 'xfs']
 }
 
+disk_pool = {
+    'type': 'string',
+    'pattern': '^\w+:\w+$'
+}
 
 disk_list = {
     'type': 'array',
