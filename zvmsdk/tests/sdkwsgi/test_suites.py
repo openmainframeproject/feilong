1c1b83a75fc1defb95b949af603a94c13a1b0117
Huang Rui <bjhuangr@cn.ibm.com>
2018-01-24 10:20:36 +0800

    add bvt test suite
    The test suite will cover guest create/delete, host get info,
    image create/delete, get version and vswitch create/delete ...
    
    Change-Id: Ibe33cbedd2cfac082a8c0ead3dc7c02d7e85ea5d
    (cherry picked from commit 4a058ea7e68ef454ed64b484ccb600fcfa5ef7af)

---
+++ zvmsdk/tests/sdkwsgi/test_suites.py
@@ -0,0 +1,32 @@
+# Copyright 2017 IBM Corp.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import unittest
+
+from zvmsdk.tests.sdkwsgi import test_guest
+from zvmsdk.tests.sdkwsgi import test_host
+from zvmsdk.tests.sdkwsgi import test_image
+from zvmsdk.tests.sdkwsgi import test_version
+
+
+bvt_testcases = [
+    test_guest.GuestHandlerTestCase("test_guest_create_delete"),
+    test_host.HostTestCase("test_host_info"),
+    test_image.ImageTestCase("test_image_create_delete"),
+    test_version.VersionTestCase("test_version"),
+]
+
+
+bvt = unittest.TestSuite()
+bvt.addTests(bvt_testcases)
