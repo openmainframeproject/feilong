cf4fca2d950f0cefe38194287f263a707f1a4a08
dongyan yang <dyyang@cn.ibm.com>
2018-02-02 15:08:02 +0800

    add fvt case of monitor cache
    Change-Id: I409149349fa076ec8d045534d2609a9b7468c031
    (cherry picked from commit d5241e07058c61148ccdd525c9cbb25f7b1ba1f6)

--- zvmsdk/tests/sdkwsgi/test_monitor.py
+++ zvmsdk/tests/sdkwsgi/test_monitor.py
@@ -12,9 +12,15 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+import threading
+import time
 
 from zvmsdk.tests.sdkwsgi import base
 from zvmsdk.tests.sdkwsgi import test_utils
+from zvmsdk import config
+from zvmsdk import monitor
+
+CONF = config.CONF
 
 
 class MonitorTestCase(base.ZVMConnectorBaseTestCase):
@@ -38,3 +44,57 @@ class MonitorTestCase(base.ZVMConnectorBaseTestCase):
 
     def test_monitor(self):
         self.assertEqual(True, True)
+
+
+class MeteringCacheTestCase(base.ZVMConnectorBaseTestCase):
+
+    def setUp(self):
+        self.set_conf('monitor', 'cache_interval', 1000)
+        self.mc = monitor.MeteringCache(('typeA', 'typeB'))
+        self.addCleanup(self.mc._reset(('typeA', 'typeB')))
+
+    def test_init(self):
+        self.assertListEqual(self.mc._cache.keys(), ['typeA', 'typeB'])
+        self.assertEqual(self.mc._cache['typeA'].keys(),
+                         ['expiration', 'data'])
+        self.assertEqual(self.mc._cache['typeB'].keys(),
+                         ['expiration', 'data'])
+        self.assertEqual(self.mc._cache['typeA']['data'], {})
+        self.assertEqual(self.mc._cache['typeB']['data'], {})
+        self.assertIsInstance(self.mc._lock, threading.RLock)
+        self.assertListEqual(self.mc._types, ['typeA', 'typeB'])
+
+    def test_set_get_delete(self):
+        self.mc.set('typeA', 'data1', 'value1')
+        self.mc.set('typeA', 'data2', 'value2')
+        self.mc.set('typeB', 'data1', 'value1')
+        self.mc.set('typeB', 'data2', 'value2')
+        self.assertListEqual(self.mc._cache['typeA']['data'].keys(),
+                         ['data1', 'data2'])
+        self.assertListEqual(self.mc._cache['typeB']['data'].keys(),
+                         ['data1', 'data2'])
+        self.assertListEqual(self.mc._cache['typeA']['data']['data1'],
+                             'value1')
+        self.assertListEqual(self.mc._cache['typeB']['data']['data1'],
+                             'value1')
+        # Test get
+        self.assertEqual(self.mc.get('typeA', 'data1'), 'value1')
+        self.assertEqual(self.mc.get('typeA', 'data2'), 'value2')
+        # Delete
+        self.mc.delete('typeA', 'data1')
+        self.assertEqual(self.mc.get('typeA', 'data1'), None)
+        self.assertEqual(self.mc.get('typeA', 'data2'), 'value2')
+
+    def test_refresh_and_expire(self):
+        data = {'data1': 'value1',
+                'data2': 'value2'}
+        # Test refresh
+        self.mc.refresh('typeA', data)
+        self.assertEqual(self.mc.get('typeA', 'data1'), 'value1')
+        self.assertEqual(self.mc.get('typeA', 'data2'), 'value2')
+        self.set_conf('monitor', 'cache_interval', 1)
+        # Test expire
+        self.mc.refresh('typeA', data)
+        time.sleep(2)
+        self.assertEqual(self.mc.get('typeA', 'data1'), None)
+        self.assertEqual(self.mc.get('typeA', 'data2'), None)
