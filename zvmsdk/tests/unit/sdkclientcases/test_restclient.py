d73b63d246866a56ded2f87e69edc2c4f31769e9
SharpRazor <bjcb@cn.ibm.com>
2018-02-05 05:43:21 -0500

    fix bugs of guest_inspect_stats and guest_inspect_vnics
    restclient cannot process list type of userid_list now
    after fix, userid_list can be like:
    'userid1,userid2,userid3'
    or
    ['userid1, userid2', 'userid3', 'userid4']
    
    fix bug:1747362
    
    Change-Id: I683201540a712737b839c28e373af12cbe01a707

--- zvmsdk/tests/unit/sdkclientcases/test_restclient.py
+++ zvmsdk/tests/unit/sdkclientcases/test_restclient.py
@@ -107,11 +107,27 @@ class RESTClientTestCase(unittest.TestCase):
                                    data=body, headers=header,
                                    verify=False)
 
+        fake_userid_list = ['userid1', 'userid2, userid3', 'userid4']
+        url = '/guests/stats?userid=userid1,userid2, userid3,userid4'
+        full_uri = self.base_url + url
+        self.client.call("guest_inspect_stats", fake_userid_list)
+        request.assert_called_with(method, full_uri,
+                                   data=body, headers=header,
+                                   verify=False)
+
+        fake_userid_string = 'userid1, userid2, userid3,userid4'
+        url = '/guests/stats?userid=%s' % fake_userid_string
+        full_uri = self.base_url + url
+        self.client.call("guest_inspect_stats", fake_userid_string)
+        request.assert_called_with(method, full_uri,
+                                   data=body, headers=header,
+                                   verify=False)
+
     @mock.patch.object(requests, 'request')
     @mock.patch('zvmconnector.restclient.RESTClient._get_token')
     def test_guest_inspect_vnics(self, get_token, request):
         method = 'GET'
-        url = '/guests/vnicsinfo?userid=%s' % self.fake_userid
+        url = '/guests/interfacestats?userid=%s' % self.fake_userid
         body = None
         header = self.headers
         full_uri = self.base_url + url
@@ -119,6 +135,21 @@ class RESTClientTestCase(unittest.TestCase):
         get_token.return_value = self._tmp_token()
 
         self.client.call("guest_inspect_vnics", self.fake_userid)
+        request.assert_called_with(method, full_uri,
+                                   data=body, headers=header,
+                                   verify=False)
+        fake_userid_list = ['userid1', 'userid2, userid3', 'userid4']
+        url = '/guests/interfacestats?userid=userid1,userid2, userid3,userid4'
+        full_uri = self.base_url + url
+        self.client.call("guest_inspect_vnics", fake_userid_list)
+        request.assert_called_with(method, full_uri,
+                                   data=body, headers=header,
+                                   verify=False)
+
+        fake_userid_string = 'userid1, userid2, userid3,userid4'
+        url = '/guests/interfacestats?userid=%s' % fake_userid_string
+        full_uri = self.base_url + url
+        self.client.call("guest_inspect_vnics", fake_userid_string)
         request.assert_called_with(method, full_uri,
                                    data=body, headers=header,
                                    verify=False)
