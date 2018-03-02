8a7b8a68b509c6898ad57e5b9e1429c3119866b1
SharpRazor <bjcb@cn.ibm.com>
2018-03-02 15:41:25 +0800

    add missing copyright
    Change-Id: Idf424caed9ff574be24eca9d1fae919643f01848

--- zvmsdk/returncode.py
+++ zvmsdk/returncode.py
@@ -1,6 +1,6 @@
 # Return code definition for zVM SDK
 #
-# Copyright 2017 IBM Corp.
+# Copyright 2017,2018 IBM Corp.
 #
 #    Licensed under the Apache License, Version 2.0 (the "License"); you may
 #    not use this file except in compliance with the License. You may obtain
