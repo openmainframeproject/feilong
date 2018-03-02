b0c5c6ec261d49a147465abbd971dbbd2a979b66
SharpRazor <bjcb@cn.ibm.com>
2018-03-02 15:41:25 +0800

    add missing copyright
    Change-Id: Idf424caed9ff574be24eca9d1fae919643f01848

--- zvmsdk/sdkwsgi/handlers/image.py
+++ zvmsdk/sdkwsgi/handlers/image.py
@@ -1,4 +1,6 @@
-#    licensed under the Apache License, Version 2.0 (the "License"); you may
+# Copyright 2017,2018 IBM Corp.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
 #    not use this file except in compliance with the License. You may obtain
 #    a copy of the License at
 #
@@ -9,6 +11,7 @@
 #    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 #    License for the specific language governing permissions and limitations
 #    under the License.
+
 """Handler for the image of the sdk API."""
 
 import json
