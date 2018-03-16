30453ddbbe44be4bb62e5272fb20e2da19a0daf0
Huang Rui <bjhuangr@cn.ibm.com>
2018-03-16 17:22:38 +0800

    zthin: new smcli for virtual network vswitch query byte stats
    Change-Id: I0115635162123af179e9136e86c40c602a066d4c
    (cherry picked from commit 0f457edaecc4189f56d060334c549a8f50917530)

---
+++ zthin-parts/zthin/src/vmapiVirtualNetworkVswitchQueryByteStats.c
@@ -0,0 +1,103 @@
+/**
+ * Copyright 2018 IBM Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include "smSocket.h"
+#include "vmapiVirtual.h"
+#include "smapiTableParser.h"
+#include <stdlib.h>
+#include <string.h>
+#include <netinet/in.h>
+
+#define PARSER_TABLE_NAME     Virtual_Network_Vswitch_Query_Byte_Stats_Layout
+#define OUTPUT_STRUCTURE_NAME vmApiVirtualNetworkVswitchQueryByteStatsOutput
+
+/**
+ * Virtual_Network_Vswitch_Query_Byte_Stats SMAPI interface
+ */
+int smVirtual_Network_Vswitch_Query_Byte_Stats(
+        struct _vmApiInternalContext* vmapiContextP, char * userid,
+        int passwordLength, char * password, char * targetIdentifier,
+        int keyValueCount, char ** keyValueArray,
+        vmApiVirtualNetworkVswitchQueryByteStatsOutput ** outData) {
+
+    const char * const functionName = "Virtual_Network_Vswitch_Query_Byte_Stats";
+
+    int tempSize;
+    int keyValueSize = 0;
+    int i;
+    int rc;
+    char * cursor;
+    char * targetStart[2];
+
+
+    int inputSize = 4 + 4 + strlen(functionName) + 4 + strlen(userid) + 4
+            + passwordLength + 4 + strlen(targetIdentifier);
+
+    for (i = 0; i < keyValueCount; i++) {
+        inputSize += strlen(keyValueArray[i]) + 1;
+        keyValueSize += strlen(keyValueArray[i]) + 1;
+    }
+
+    char * inputP = 0;
+    char * smapiOutputP = 0;
+    char line[LINESIZE];
+
+    // Build SMAPI input parameter buffer
+    if (0 == (inputP = malloc(inputSize))) {
+        return MEMORY_ERROR;
+    }
+    cursor = inputP;
+    PUT_INT(inputSize - 4, cursor);
+    tempSize = strlen(functionName);
+
+    PUT_INT(tempSize, cursor);
+    memcpy(cursor, functionName, tempSize);
+    cursor += tempSize;
+
+    tempSize = strlen(userid);  // UserId 1..8 or 0..8 chars
+    PUT_INT(tempSize, cursor);
+    if (tempSize > 0) {
+        memcpy(cursor, userid, tempSize);
+        cursor += tempSize;
+    }
+
+    tempSize = passwordLength;  // Password 1..200 or 0..200 chars
+    PUT_INT(tempSize, cursor);
+    if (tempSize > 0) {
+        memcpy(cursor, password, tempSize);
+        cursor += tempSize;
+    }
+
+    tempSize = strlen(targetIdentifier);  // Target identifier 1..8
+    PUT_INT(tempSize, cursor);
+    targetStart[0] = targetIdentifier;  // Remember position relocateTarget for later tracing
+    memcpy(cursor, targetIdentifier, tempSize);
+    cursor += tempSize;
+
+    for (i = 0; i < keyValueCount; i++) {  // Keyword = value terminated strings
+        targetStart[i+1] = cursor;  // Remember position of parameter for later tracing
+        tempSize = strlen(keyValueArray[i]) + 1;
+        strcpy(cursor, keyValueArray[i]);
+        cursor += tempSize;
+    }
+
+    // This routine will send SMAPI the input, delete the input storage
+    // and call the table parser to set the output in outData
+    rc = getAndParseSmapiBuffer(vmapiContextP, &inputP, inputSize,
+            PARSER_TABLE_NAME,  // Integer table
+            TO_STRING(PARSER_TABLE_NAME),  // String name of the table
+            (char * *) outData);
+    return rc;
+}
