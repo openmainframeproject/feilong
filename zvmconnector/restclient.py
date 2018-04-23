ff5fa4fbe99ff7e6b1de85468c10c1298573f9fe
dongyan yang <dyyang@cn.ibm.com>
2018-04-23 13:30:35 +0800

    add guest_live_resize_cpus API
    Change-Id: I725a9d517636f5874e5834d3441a5364096eff94

--- zvmconnector/restclient.py
+++ zvmconnector/restclient.py
@@ -213,6 +213,13 @@ def req_guest_get_console_output(start_index, *args, **kwargs):
     return url, body
 
 
+def req_guest_live_resize_cpus(start_index, *args, **kwargs):
+    url = '/guests/%s/action'
+    body = {'action': 'live_resize_cpus',
+            'cpu_cnt': args[start_index]}
+    return url, body
+
+
 def req_guest_capture(start_index, *args, **kwargs):
     url = '/guests/%s/action'
     body = {'action': 'capture',
@@ -531,6 +538,11 @@ DATABASE = {
         'args_required': 1,
         'params_path': 1,
         'request': req_guest_get_console_output},
+    'guest_live_resize_cpus': {
+        'method': 'POST',
+        'args_required': 2,
+        'params_path': 1,
+        'request': req_guest_live_resize_cpus},
     'guest_capture': {
         'method': 'POST',
         'args_required': 2,
