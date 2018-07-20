0019091a5455a7a9070bef0e555672a6caf3ccf2
jichenjc <jichenjc@cn.ibm.com>
2018-07-21 03:24:08 +0800

    Fix py3 issue
    1) Queue usage
    refer to here:
    http://python-future.org/compatible_idioms.html#queue
    
    2) byte to str transfer in zvmsdk/utils.py
    
    3) socket send result need to be encoded and decoded
    
    4) remove types.TypeList usage
    
    5) getHost use bytes.decode to decode bytes to string
    
    Change-Id: I2ad362832585fc9643c07a32b77869d4fcffa6da

--- zvmsdk/smutclient.py
+++ zvmsdk/smutclient.py
@@ -1880,7 +1880,8 @@ class SMUTClient(object):
         try:
             root_disk_size = int(output[144:156])
             disk_units = output[220:223]
-            root_disk_units = ':'.join([str(root_disk_size), disk_units])
+            root_disk_units = ':'.join([str(root_disk_size),
+                                       bytes.decode(disk_units)])
         except ValueError:
             msg = ("Image file at %s is missing built-in disk size "
                    "metadata, it was probably not captured by SDK" %
@@ -1888,6 +1889,7 @@ class SMUTClient(object):
             LOG.error(msg)
             raise exception.SDKImageOperationError(rs=6)
 
+        output = bytes.decode(output)
         if 'FBA' not in output and 'CKD' not in output:
             raise exception.SDKImageOperationError(rs=7)
 
@@ -1938,7 +1940,7 @@ class SMUTClient(object):
         """Calculate the md5sum of the specific image file"""
         try:
             current_md5 = hashlib.md5()
-            if isinstance(fpath, basestring) and os.path.exists(fpath):
+            if isinstance(fpath, six.string_types) and os.path.exists(fpath):
                 with open(fpath, "rb") as fh:
                     for chunk in self._read_chunks(fh):
                         current_md5.update(chunk)
