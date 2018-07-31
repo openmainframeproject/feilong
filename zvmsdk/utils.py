bb3c98307ff256e89ebe56d58c8665d255b6e477
jichenjc <jichenjc@cn.ibm.com>
2018-07-31 10:45:59 +0800

    fix py3 issues including following items:
    1) Queue usage
    refer to here:
    http://python-future.org/compatible_idioms.html#queue
    
    2) byte to str transfer in zvmsdk/utils.py
    
    3) socket send result need to be encoded and decoded
    
    4) remove types.TypeList usage
    
    5) getHost use bytes.decode to decode bytes to string
    
    Change-Id: I2ad362832585fc9643c07a32b77869d4fcffa6da

--- zvmsdk/utils.py
+++ zvmsdk/utils.py
@@ -63,6 +63,7 @@ def execute(cmd):
         err_msg = ('Command "%s" Error: %s' % (' '.join(cmd), str(err)))
         raise exception.SDKInternalError(msg=err_msg)
 
+    output = bytes.decode(output)
     return (rc, output)
 
 
@@ -192,7 +193,7 @@ def to_utf8(text):
     if isinstance(text, bytes):
         return text
     elif isinstance(text, six.text_type):
-        return text.encode('utf-8')
+        return text.encode()
     else:
         raise TypeError("bytes or Unicode expected, got %s"
                         % type(text).__name__)
@@ -440,7 +441,9 @@ def get_smut_userid():
     try:
         userid = subprocess.check_output(cmd,
                                          close_fds=True,
-                                         stderr=subprocess.STDOUT).split()[0]
+                                         stderr=subprocess.STDOUT)
+        userid = bytes.decode(userid)
+        userid = userid.split()[0]
         return userid
     except Exception as err:
         msg = ("Could not find the userid of the smut server: %s") % err
@@ -458,11 +461,17 @@ def get_namelist():
         if len(CONF.zvm.namelist) <= 8:
             return CONF.zvm.namelist
 
-    return ''.join(('NL', get_smut_userid().rjust(6, '0')[-6:]))
+    # return ''.join(('NL', get_smut_userid().rjust(6, '0')[-6:]))
+    # py3 compatible changes
+    userid = get_smut_userid().encode()
+    return 'NL' + userid.rjust(6, '0')[-6:]
 
 
 def generate_iucv_authfile(fn, client):
     """Generate the iucv_authorized_userid file"""
+    #NOTE: client might be bytes, so need decode in order to remove 'b' prefix
+    client = bytes.decode(client)
+    
     lines = ['#!/bin/bash\n',
              'echo -n %s > /etc/iucv_authorized_userid\n' % client]
     with open(fn, 'w') as f:
@@ -515,9 +524,9 @@ def make_dummy_image(image_path, d_type='CKD'):
     header = ("z/VM %(type)s Disk Image:           0 %(unit)s" %
               {'type': d_type, 'unit': d_unit})
 
-    header = bytes(' '.join((header, 'HLen: 0055', 'GZIP: 0')))
+    header = (' '.join((header, 'HLen: 0055', 'GZIP: 0')))
     with open(image_path, 'wb') as f:
-        f.write(header)
+        f.write(header.encode())
 
 
 @contextlib.contextmanager
