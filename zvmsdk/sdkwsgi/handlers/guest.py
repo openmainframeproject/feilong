3a4b6d48bccd79c27121c86c8db55a8ba86173b2
XiaoMeiZheng <xmzheng@cn.ibm.com>
2018-08-01 11:28:48 +0800

    Add restapi for pre-migrate.
    Includes restclient, restapi, fvt, bvt.
    
    Change-Id: I33143b29b776798dfdbb75ac055fc79bc7929c65

--- zvmsdk/sdkwsgi/handlers/guest.py
+++ zvmsdk/sdkwsgi/handlers/guest.py
@@ -268,6 +268,11 @@ class VMAction(object):
 
         return info
 
+    @validation.schema(guest.pre_migrate_vm)
+    def register_vm(self, userid, body):
+        info = self.client.send_request('guest_pre_migrate', userid)
+        return info
+
     @validation.schema(guest.live_migrate_vm)
     def live_migrate_vm(self, userid, body):
         destination = body['destination']
