trenxiaof/python-zvm-sdk

Add sdkwsgi interfaces to handle getting guest power state from hypervisor
Signed-off-by: Xiao Feng Ren <renxiaof@linux.vnet.ibm.com>

zvmsdk/sdkwsgi/handlers/host.py
@@ -42,6 +42,11 @@ class HostAction(object):
         info = self.client.send_request('host_get_info')
         return info
 
+    @validation.query_schema(host.userid_list_query)
+    def get_power_state(self, req, userid):
+        info = self.client.send_request('host_get_guest_power_state', userid)
+        return info
+
     @validation.query_schema(image.diskpool)
     def diskpool_get_info(self, req, poolname):
         info = self.client.send_request('host_diskpool_get_info',
@@ -70,6 +75,25 @@ def host_get_info(req):
     req.response.body = utils.to_utf8(info_json)
     req.response.content_type = 'application/json'
     return req.response
+
+
+@util.SdkWsgify
+@tokens.validate
+def host_get_guest_power_state(req):
+
+    def _host_get_guest_power_state(req, userid):
+        action = get_action()
+        return action.get_power_state(req, userid)
+
+    userid = util.wsgi_path_item(req.environ, 'userid')
+    info = _host_get_guest_power_state(req, userid)
+
+    info_json = json.dumps(info)
+    req.response.status = util.get_http_code_from_sdk_return(info,
+        additional_handler=util.handle_not_found)
+    req.response.body = utils.to_utf8(info_json)
+    req.response.content_type = 'application/json'
+    return req.respons
 
 
 @util.SdkWsgify

openmainframeproject/feilong

zvmsdk/sdkwsgi/handlers/host.py
@@ -24,6 +24,7 @@ from zvmsdk.sdkwsgi import util
 from zvmsdk import utils
 from zvmsdk.sdkwsgi import validation
 from zvmsdk.sdkwsgi.schemas import image
+from zvmsdk.sdkwsgi.schemas import host
 
 
 _HOSTACTION = None
@@ -40,6 +41,11 @@ class HostAction(object):
 
     def get_info(self):
         info = self.client.send_request('host_get_info')
+        return info
+
+    @validation.query_schema(host.userid_list_query)
+    def get_power_state(self, req, userid):
+        info = self.client.send_request('host_get_guest_power_state', userid)
         return info
 
     @validation.query_schema(image.diskpool)
@@ -67,6 +73,25 @@ def host_get_info(req):
     info = _host_get_info()
     info_json = json.dumps(info)
     req.response.status = util.get_http_code_from_sdk_return(info)
+    req.response.body = utils.to_utf8(info_json)
+    req.response.content_type = 'application/json'
+    return req.response
+
+
+@util.SdkWsgify
+@tokens.validate
+def host_get_guest_power_state(req):
+
+    def _host_get_guest_power_state(req, userid):
+        action = get_action()
+        return action.get_power_state(req, userid)
+
+    userid = util.wsgi_path_item(req.environ, 'userid')
+    info = _host_get_guest_power_state(req, userid)
+
+    info_json = json.dumps(info)
+    req.response.status = util.get_http_code_from_sdk_return(info,
+        additional_handler=util.handle_not_found)
     req.response.body = utils.to_utf8(info_json)
     req.response.content_type = 'application/json'
     return req.response