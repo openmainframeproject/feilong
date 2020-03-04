trenxiaof/python-zvm-sdk

add REST api to get adapters information
now support get following information of a adapter:
1. adapter_address
2. adapter_status
3. lan_owner
4. lan_name
5. mac_address
6. mac_ip_address: this may not found
7. mac_ip_version: this may not found
And add unit test cases and Doc of REST API

zvmsdk/tests/unit/test_api.py
@@ -41,6 +41,11 @@ class SDKAPITestCase(base.SDKTestCase):
         self.api.guest_get_info(self.userid)
         ginfo.assert_called_once_with(self.userid)
 
+    @mock.patch("zvmsdk.vmops.VMOps.get_adapters_info")
+    def test_guest_get_info(self, adapters_info):
+        self.api.guest_get_adapters_info(self.userid)
+        adapters_info.assert_called_once_with(self.userid)
+
     @mock.patch("zvmsdk.vmops.VMOps.guest_deploy")
     def test_guest_deploy(self, guest_deploy):
         user_id = 'fakevm'

openmainframeproject/feilong

Signed-off-by: SharpRazor <bjcb@cn.ibm.com>

zvmsdk/tests/unit/test_api.py
@@ -41,6 +41,11 @@ class SDKAPITestCase(base.SDKTestCase):
         self.api.guest_get_info(self.userid)
         ginfo.assert_called_once_with(self.userid)
 
+    @mock.patch("zvmsdk.vmops.VMOps.get_adapters_info")
+    def test_guest_get_adapters_info(self, adapters_info):
+        self.api.guest_get_adapters_info(self.userid)
+        adapters_info.assert_called_once_with(self.userid)
+
     @mock.patch("zvmsdk.vmops.VMOps.guest_deploy")
     def test_guest_deploy(self, guest_deploy):
         user_id = 'fakevm'