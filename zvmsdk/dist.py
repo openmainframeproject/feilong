dongyanyang/python-zvm-sdk

add api to extend root partition of  volume
Cann't rely on cloud-init to grow partition and resizefs when the root
partition is on multipath disk.
So add a new API to punch script filt to guest to do the partition grow.

Signed-off-by: dyyang <dyyang@cn.ibm.com>

zvmsdk/dist.py
@@ -595,6 +595,11 @@ class LinuxDist(object):
         template = env.get_template(template_name)
         return template
 
+    def get_extend_partition_cmds(self):
+        template = self.get_template("vmactions", "grow_root_partition.j2")
+        content = template.render()
+        return content
+
 
 class rhel(LinuxDist):
     def _get_network_file_path(self):

zvmsdk/tests/unit/sdkclientcases/test_restclient.py
@@ -903,3 +903,22 @@ class RESTClientTestCase(unittest.TestCase):
         request.assert_called_with(method, full_uri,
                                    data=body, headers=header,
                                    verify=False)
+
+    @mock.patch.object(requests, 'request')
+    @mock.patch('zvmconnector.restclient.RESTClient._get_token')
+    def test_guest_grow_partition(self, get_token, request):
+        method = 'POST'
+        url = '/guests/%s/action' % self.fake_userid
+        body = {'action': 'grow_partition',
+                'os_version': 'RHEL7.8'}
+        body = json.dumps(body)
+        header = self.headers
+        full_uri = self.base_url + url
+        request.return_value = self.response
+        get_token.return_value = self._tmp_token()
+
+        self.client.call("guest_grow_partition", self.fake_userid,
+                         'RHEL7.8')
+        request.assert_called_with(method, full_uri,
+                                   data=body, headers=header,
+                                   verify=False)

zvmsdk/tests/unit/sdkwsgi/handlers/test_guest.py
@@ -350,6 +350,18 @@ class GuestActionsTest(SDKWSGITest):
 
     @mock.patch.object(util, 'wsgi_path_item')
     @mock.patch('zvmconnector.connector.ZVMConnector.send_request')
+    def test_guest_grow_partition(self, mock_action, mock_userid):
+        self.req.body = """{"action": "grow_partition",
+                            "os_version": "RHEL7.8"}"""
+        mock_action.return_value = ''
+        mock_userid.return_value = FAKE_USERID
+
+        guest.guest_action(self.req)
+        mock_action.assert_called_once_with('guest_grow_partition',
+                                            FAKE_USERID, "RHEL7.8")
+
+    @mock.patch.object(util, 'wsgi_path_item')
+    @mock.patch('zvmconnector.connector.ZVMConnector.send_request')
     def test_guest_deploy(self, mock_action,
                           mock_userid):
         self.req.body = """{"action": "deploy",

zvmsdk/tests/unit/test_vmops.py
@@ -14,7 +14,9 @@
 
 
 import mock
+import tempfile
 
+from zvmsdk import dist
 from zvmsdk import exception
 from zvmsdk import vmops
 from zvmsdk.tests.unit import base
@@ -370,3 +372,24 @@ class SDKVMOpsTestCase(base.SDKTestCase):
         self.vmops.live_migrate_vm(userid, destination, parms, action)
         power_state.assert_called_once_with(userid)
         live_migrate_vm.assert_called_once_with(userid, destination)
+
+    @mock.patch('zvmsdk.smtclient.SMTClient.punch_file')
+    @mock.patch('zvmsdk.utils.PathUtils.get_guest_temp_path')
+    @mock.patch.object(dist.rhel7, 'get_extend_partition_cmds')
+    @mock.patch("zvmsdk.dist.LinuxDistManager.get_linux_dist")
+    def test_guest_grow_partition(self, get_dist, get_dist_cmds,
+                                  tmp_path, punch_file):
+        userid = "FAKE_USERID"
+        os_version = "RHEL7.8"
+        get_dist.return_value = dist.rhel7
+        get_dist_cmds.return_value = "fake_cmds"
+        tmp_inst_dir = tempfile.mkdtemp(prefix=userid,
+                                        dir='/tmp')
+        tmp_path.return_value = tmp_inst_dir
+        self.vmops.guest_grow_partition(userid, os_version)
+        get_dist.assert_called_once_with(os_version)
+        get_dist_cmds.assert_called_once_with()
+        tmp_path.assert_called_once_with(userid)
+        punch_file.assert_called_once_with(userid,
+                                           ("%s/gpartvol.sh" % tmp_inst_dir),
+                                           "X")

zvmsdk/vmops.py
@@ -15,6 +15,7 @@
 
 import os
 import six
+import shutil
 
 from zvmsdk import config
 from zvmsdk import dist
@@ -228,6 +229,26 @@ class VMOps(object):
             LOG.info("Complete configure disks for vm: %s", userid)
         else:
             LOG.info("No disk to handle on %s." % userid)
+
+    def guest_grow_partition(self, userid, os_version):
+        """ Punch the grow partition script to the target guest. """
+        LOG.debug('Begin to punch grow partition commands to guest: %s',
+                  userid)
+        linuxdist = self._dist_manager.get_linux_dist(os_version)()
+        # get configuration commands
+        config_cmds = linuxdist.get_extend_partition_cmds()
+        # Creating tmp file with these cmds
+        temp_folder = self._pathutils.get_guest_temp_path(userid)
+        file_path = os.path.join(temp_folder, 'gpartvol.sh')
+        LOG.debug('Creating file %s to contain root partition extension '
+                  'commands' % file_path)
+        with open(file_path, "w") as f:
+            f.write(config_cmds)
+        try:
+            self._smtclient.punch_file(userid, file_path, "X")
+        finally:
+            LOG.debug('Removing the folder %s ', temp_folder)
+            shutil.rmtree(temp_folder)
 
     def is_powered_off(self, instance_name):
         """Return True if the instance is powered off."""

openmainframeproject/feilong

zvmsdk/dist.py
@@ -595,6 +595,11 @@ class LinuxDist(object):
         template = env.get_template(template_name)
         return template
 
+    def get_extend_partition_cmds(self):
+        template = self.get_template("vmactions", "grow_root_volume.j2")
+        content = template.render()
+        return content
+
 
 class rhel(LinuxDist):
     def _get_network_file_path(self):

zvmsdk/tests/unit/sdkclientcases/test_restclient.py
@@ -903,3 +903,22 @@ class RESTClientTestCase(unittest.TestCase):
         request.assert_called_with(method, full_uri,
                                    data=body, headers=header,
                                    verify=False)
+
+    @mock.patch.object(requests, 'request')
+    @mock.patch('zvmconnector.restclient.RESTClient._get_token')
+    def test_guest_grow_root_volume(self, get_token, request):
+        method = 'POST'
+        url = '/guests/%s/action' % self.fake_userid
+        body = {'action': 'grow_root_volume',
+                'os_version': 'RHEL7.8'}
+        body = json.dumps(body)
+        header = self.headers
+        full_uri = self.base_url + url
+        request.return_value = self.response
+        get_token.return_value = self._tmp_token()
+
+        self.client.call("guest_grow_root_volume", self.fake_userid,
+                         'RHEL7.8')
+        request.assert_called_with(method, full_uri,
+                                   data=body, headers=header,
+                                   verify=False)

zvmsdk/tests/unit/sdkwsgi/handlers/test_guest.py
@@ -350,6 +350,18 @@ class GuestActionsTest(SDKWSGITest):
 
     @mock.patch.object(util, 'wsgi_path_item')
     @mock.patch('zvmconnector.connector.ZVMConnector.send_request')
+    def test_guest_grow_root_volume(self, mock_action, mock_userid):
+        self.req.body = """{"action": "grow_root_volume",
+                            "os_version": "RHEL7.8"}"""
+        mock_action.return_value = ''
+        mock_userid.return_value = FAKE_USERID
+
+        guest.guest_action(self.req)
+        mock_action.assert_called_once_with('guest_grow_root_volume',
+                                            FAKE_USERID, "RHEL7.8")
+
+    @mock.patch.object(util, 'wsgi_path_item')
+    @mock.patch('zvmconnector.connector.ZVMConnector.send_request')
     def test_guest_deploy(self, mock_action,
                           mock_userid):
         self.req.body = """{"action": "deploy",

zvmsdk/tests/unit/test_vmops.py
@@ -14,7 +14,9 @@
 
 
 import mock
+import tempfile
 
+from zvmsdk import dist
 from zvmsdk import exception
 from zvmsdk import vmops
 from zvmsdk.tests.unit import base
@@ -370,3 +372,24 @@ class SDKVMOpsTestCase(base.SDKTestCase):
         self.vmops.live_migrate_vm(userid, destination, parms, action)
         power_state.assert_called_once_with(userid)
         live_migrate_vm.assert_called_once_with(userid, destination)
+
+    @mock.patch('zvmsdk.smtclient.SMTClient.punch_file')
+    @mock.patch('zvmsdk.utils.PathUtils.get_guest_temp_path')
+    @mock.patch.object(dist.rhel7, 'get_extend_partition_cmds')
+    @mock.patch("zvmsdk.dist.LinuxDistManager.get_linux_dist")
+    def test_guest_grow_root_volume(self, get_dist, get_dist_cmds,
+                                  tmp_path, punch_file):
+        userid = "FAKE_USERID"
+        os_version = "RHEL7.8"
+        get_dist.return_value = dist.rhel7
+        get_dist_cmds.return_value = "fake_cmds"
+        tmp_inst_dir = tempfile.mkdtemp(prefix=userid,
+                                        dir='/tmp')
+        tmp_path.return_value = tmp_inst_dir
+        self.vmops.guest_grow_root_volume(userid, os_version)
+        get_dist.assert_called_once_with(os_version)
+        get_dist_cmds.assert_called_once_with()
+        tmp_path.assert_called_once_with(userid)
+        punch_file.assert_called_once_with(userid,
+                                           ("%s/gpartvol.sh" % tmp_inst_dir),
+                                           "X")

zvmsdk/vmops.py
@@ -15,6 +15,7 @@
 
 import os
 import six
+import shutil
 
 from zvmsdk import config
 from zvmsdk import dist
@@ -228,6 +229,26 @@ class VMOps(object):
             LOG.info("Complete configure disks for vm: %s", userid)
         else:
             LOG.info("No disk to handle on %s." % userid)
+
+    def guest_grow_root_volume(self, userid, os_version):
+        """ Punch the grow partition script to the target guest. """
+        LOG.debug('Begin to punch grow partition commands to guest: %s',
+                  userid)
+        linuxdist = self._dist_manager.get_linux_dist(os_version)()
+        # get configuration commands
+        config_cmds = linuxdist.get_extend_partition_cmds()
+        # Creating tmp file with these cmds
+        temp_folder = self._pathutils.get_guest_temp_path(userid)
+        file_path = os.path.join(temp_folder, 'gpartvol.sh')
+        LOG.debug('Creating file %s to contain root partition extension '
+                  'commands' % file_path)
+        with open(file_path, "w") as f:
+            f.write(config_cmds)
+        try:
+            self._smtclient.punch_file(userid, file_path, "X")
+        finally:
+            LOG.debug('Removing the folder %s ', temp_folder)
+            shutil.rmtree(temp_folder)
 
     def is_powered_off(self, instance_name):
         """Return True if the instance is powered off."""