a6122d20db2fa08aa449ea703959590a582791bf
dyyang <dyyang@cn.ibm.com>
2021-11-25 19:53:50 +0800

    return the total fcp paths count in get_volume_connector
    so that in the upper layer we can check whether the allocated FCP
    count equals to the configured total path count. If not equal,
    we then can log and send warning msg to end user.
    
    Signed-off-by: dyyang <dyyang@cn.ibm.com>

--- zvmsdk/tests/unit/test_volumeop.py
+++ zvmsdk/tests/unit/test_volumeop.py
@@ -837,7 +837,8 @@ class TestFCPVolumeManager(base.SDKTestCase):
                         'wwpns': ['2007123400001234'],
                         'phy_to_virt_initiators': {'2007123400001234':
                             '20076d8500005181'},
-                        'host': 'fakehost'}
+                        'host': 'fakehost',
+                        'fcp_paths': 1}
             self.assertEqual(expected, connections)
 
             fcp_list = self.db_op.get_from_fcp('b83c')
